4b3754c340c646561e8fb15005e6fe690daef821
Shaun Wallace <shaun_wallace@icerm.brown.edu>
2016-03-02 20:46:49 -0500

    New UserInterest Services

---
+++ src/main/java/drafty/services/UserInterestService.java
@@ -0,0 +1,86 @@
+package drafty.services;
+
+public class UserInterestService implements UserInterestServiceImpl {
+
+	
+	@Override
+	public void genUserInterest(String user_id) {
+		genUserIntUni(user_id);
+		genUserIntJoinYear(user_id);
+		genUserIntRank(user_id);
+		genUserIntSubfield(user_id);
+		genUserIntBach(user_id);
+		genUserIntMast(user_id);
+		genUserIntDoct(user_id);
+		genUserIntPostDoc(user_id);
+		genUserIntGender(user_id);
+		genUserIntPhotoUrl(user_id);
+		genUserIntSources(user_id);
+	}
+
+	@Override
+	public void genUserIntUni(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntJoinYear(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntRank(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntSubfield(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntBach(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntMast(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntDoct(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntPostDoc(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntGender(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntPhotoUrl(String user_id) {
+		
+		
+	}
+
+	@Override
+	public void genUserIntSources(String user_id) {
+		
+		
+	}
+}
