318ff38b43986b4a3131531290ea7def4dc550b3
pengzhengyi <zhengyi_peng@brown.edu>
2019-10-04 18:01:56 -0400

    (1) add dot env variables for mariadb (2) create database interface template in mysql.ts

---
+++ backend/src/util/mysql.ts
@@ -0,0 +1,66 @@
+import mariadb from "mariadb";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } from "./secrets";
+
+/**
+ * This file provides interface to MySQL (MariaDB database), it will:
+ *    - open a connection
+ *    - define all operations relevant with the database
+ *      <a href="https://mariadb.com/kb/en/library/connector-nodejs-promise-api/">mariadb Node.js Promise API</a>
+ *
+ *  In the end, you want to export all database operations so that it can be called from controllers.
+ */
+
+// create a pool of connection to database
+const pool = mariadb.createPool({
+   user: DB_USER,
+   password: DB_PASSWORD,
+   host: DB_HOST,
+  database: DB_DATABASE,
+  // connectionLimit: 5, // Maximum number of connection in pool.
+});
+
+
+// DATABASE OPERATIONS
+
+// Provide one example of declaring one database operation using async / await
+// TODO remove this when using for production
+async function testConnection() {
+  // pool.query is a shortcut to get a connection from pool, execute a query and release connection.
+  // demonstrates a basic query
+  await pool.query("SELECT NOW()")
+      .then(rows => console.log(rows));
+
+  // demonstrates how to write a multi-line query
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS books (
+      BookID INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+      Title VARCHAR(100) NOT NULL,
+      SeriesID INT, AuthorID INT)
+    `
+  );
+  // pool.batch is a shortcut to get a connection from pool, execute a batch and release connection.
+  // demonstrates bulk inserts
+  await pool.batch(`
+    INSERT INTO books (Title,SeriesID,AuthorID) VALUES (?, ?, ?, ?)
+  `, [
+       ["The Fellowship of the Ring",1,1],
+       ["The Two Towers",1,1],
+       ["The Return of the King",1,1],
+       ["The Sum of All Men",2,2],
+       ["Brotherhood of the Wolf",2,2],
+       ["Wizardborn",2,2],
+       ["The Hobbbit",0,1]
+  ]).then(
+    res => console.log(res)
+  );
+
+  await pool.query("DROP TABLE IF EXISTS books");
+}
+
+// testsing everything works
+// TODO remove this when using for production
+testConnection();
+
+
+// exports
+export { testConnection as databaseTestFunctionality };
--- backend/src/util/secrets.ts
+++ backend/src/util/secrets.ts
@@ -3,28 +3,68 @@ import dotenv from "dotenv";
 import fs from "fs";
 
 if (fs.existsSync(".env")) {
-    logger.debug("Using .env file to supply config environment variables");
-    dotenv.config({ path: ".env" });
+  // synchronously look for .env file in file system
+  logger.info("Using .env file to supply config environment variables");
+  dotenv.config({ path: ".env" });
+} else if (fs.existsSync(".env.example")) {
+  // synchronously look for .env.example file in file system
+  logger.warn("Using .env.example file to supply config environment variables -- this will often not work, please supply an .env file!");
+  dotenv.config({ path: ".env.example" });
 } else {
-    logger.debug("Using .env.example file to supply config environment variables");
-    dotenv.config({ path: ".env.example" });  // you can delete this after you create your own .env file!
+  logger.error("Both .env file and .env.example file do not exist! Please supply a .env file");
+  process.exit(1);
 }
+
 export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
-
 if (!SESSION_SECRET) {
     logger.error("No client secret. Set SESSION_SECRET environment variable.");
     process.exit(1);
 }
 
-if (!MONGODB_URI) {
-    if (prod) {
-        logger.error("No mongo connection string. Set MONGODB_URI environment variable.");
-    } else {
-        logger.error("No mongo connection string. Set MONGODB_URI_LOCAL environment variable.");
-    }
-    process.exit(1);
+/**
+ * Handle the case where a variable cannot be found in dot environment.
+ *
+ * Args:
+ *    variableName: Name of the variable trying to resolve.
+ * Returns:
+ *    None, will exit.
+ */
+function defaultVariableNotFoundHandler(variableName: string) {
+  logger.error(`Cannot find ${variableName} in dot environment!`);
+  process.exit(1);
 }
+
+/**
+ * Resolves a variable by doing a lookup in dot environment.
+ *
+ * Args:
+ *    variableName: name of the variable in production environment.
+ *    variableLocalName: name of the variable in environment other than production (dev, test for example).
+ *    variableNotFoundHandler: A callback function that will be invoked with the name of the variable as the only argument when the variable is not found in the dot environment.
+ * Returns:
+ *    The value of the variable specified by variableName in dot environment.
+ */
+   function resolveEnvironmentVariable(
+     variableName: string,
+     variableLocalName=`${variableName}_LOCAL`,
+     variableNotFoundHandler=defaultVariableNotFoundHandler): string {
+  const name: string = prod ? variableName : variableLocalName;
+  const value: string = process.env[name];
+  if (value) {
+    console.log(`${variableName} = ${value}`);
+    return value;
+  } else {
+    variableNotFoundHandler(name);
+  }
+}
+
+
+// Dot environment variables
+export const MONGODB_URI = resolveEnvironmentVariable("MONGODB_URI");
+export const DB_HOST = resolveEnvironmentVariable("DB_HOST");
+export const DB_USER = resolveEnvironmentVariable("DB_USER");
+export const DB_PASSWORD = resolveEnvironmentVariable("DB_PASSWORD");
+export const DB_DATABASE = resolveEnvironmentVariable("DB_DATABASE");
