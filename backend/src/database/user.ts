a72b404c8ade04d71ef9525917d3883db26832be
Shaun Wallace <shaun_wallace@brown.edu>
2019-10-24 11:11:31 -0400

    change UserRow to UserModel

--- backend/src/database/user.ts
+++ backend/src/database/user.ts
@@ -1,4 +1,4 @@
-import { db } from "./mysql";
+import { db, logDbErr } from "./mysql";
 import async from "async";
 
 // DATABASE Table Names and Field Names
@@ -10,9 +10,9 @@ export const emailFieldName    = "email";
 export const passwordFieldName = "password";
 
 // supported fields that can be used to look up a user
-const validFieldNamesForUserLookup = [idFieldName, usernameFieldName, emailFieldName];
+const validFieldNamesForLookup = [idFieldName, usernameFieldName, emailFieldName];
 
-export interface UserRow {
+export interface UserModel {
   [idFieldName]: number;
   [usernameFieldName]: string;
   [emailFieldName]: String;
@@ -20,36 +20,36 @@ export interface UserRow {
 }
 
 // Result type of findUserByField
-export type findUserByFieldResultType = UserRow | null | undefined;
+export type findUserByFieldResultType = UserModel | null | undefined;
 export type findUserByFieldCallbackType = (error: Error | null, user?: findUserByFieldResultType) => any;
 /**
  * Try to find a user by table field.
  *
  * Args:
- *    fieldName: the table field name used in lookup, must be a member of validFieldNamesForUserLookup.
+ *    fieldName: the table field name used in lookup, must be a member of validFieldNamesForLookup.
  *    fieldValue: the value of specified field name to match, must be either a number or a string.
  *    callback: a callback function that will receive [error, userRow] as arguments
  *      - if the query succeeds, error is none and the userRow is the first row of results
  *      - if the query fails, error is a custom Error about user with specified field name and value cannot not found and userRow is null
  *      - if the query throws an error, error is the original error object
- *      - if the field name is not a member of validFieldNamesForUserLookup, error is custom Error about unsupported field name
+ *      - if the field name is not a member of validFieldNamesForLookup, error is custom Error about unsupported field name
  * Returns:
  *    None, callback will be called
  */
 export async function findUserByField(fieldName: string, fieldValue: string | number, callback: findUserByFieldCallbackType) {
-  if (!validFieldNamesForUserLookup.includes(fieldName)) {
+  if (!validFieldNamesForLookup.includes(fieldName)) {
       callback(new Error(`Cannot look up a user using field - ${fieldName}`));
   }
   try {
     const [rows] = await db.query("SELECT * FROM ?? WHERE ?? = ?", [userTableName, fieldName, fieldValue]);
     if (Array.isArray(rows) && rows.length > 0) {
-      const userRow = rows[0] as UserRow;
+      const userRow = rows[0] as UserModel;
       callback(null, userRow);
     } else {
       callback(new Error(`Cannot find a user whose ${fieldName} is ${fieldValue}`), null);
     }
   } catch (error) {
-    db.logDatabaseError(error, "error during finding user", "warn");
+    logDbErr(error, "error during finding user", "warn");
     callback(error);
   }
 }
@@ -66,12 +66,12 @@ export async function findUserByField(fieldName: string, fieldValue: string | nu
  * Returns:
  *    None, callback will be called.
  */
-export async function createUser(user: Partial<UserRow>, callback: Function) {
+export async function createUser(user: Partial<UserModel>, callback: Function) {
   try {
     const [results, fields] = await db.query("INSERT INTO ?? SET ?", [userTableName, user]);
     callback(null, results, fields);
   } catch (error) {
-    db.logDatabaseError(error, "error during creating user", "warn");
+    logDbErr(error, "error during creating user", "warn");
     callback(error);
   }
 }
@@ -88,34 +88,12 @@ export async function createUser(user: Partial<UserRow>, callback: Function) {
  * Returns:
  *    None, callback will be called.
  */
-export async function updateUser(updates: Partial<UserRow>, constraints: Partial<UserRow>, callback: Function) {
+export async function updateUser(updates: Partial<UserModel>, constraints: Partial<UserModel>, callback: Function) {
   try {
     const [results, fields] = await db.query("UPDATE ?? SET ? WHERE ?", [userTableName, updates, constraints]);
     callback(null, results, fields);
   } catch (error) {
-    db.logDatabaseError(error, "error during updating existing user", "warn");
-    callback(error);
-  }
-}
-
-/**
- * Deletes an existing user in database.
- *
- * Args:
- *    constraints: an object of equalities between field name and field value, only the row matching
- *      these constraints will be deleted.
- *    callback: A callback function that will either
- *      - receive [error] if the insertion fails
- *      - receive [null, results, fields] if the delete succeeds
- * Returns:
- *    None, callback will be called.
- */
-export async function deleteUser(constraints: Partial<UserRow>, callback: Function) {
-  try {
-    const [results, fields] = await db.query("DELETE FROM ?? WHERE ? LIMIT 1", [userTableName, constraints]);
-    callback(null, results, fields);
-  } catch (error) {
-    db.logDatabaseError(error, "error during deleting user", "warn");
+    logDbErr(error, "error during updating existing user", "warn");
     callback(error);
   }
 }
