0c118f087a35cc8996177da5d8814eac0f88a3d0
pengzhengyi <zhengyi_peng@brown.edu>
2019-11-01 18:33:52 -0400

    rewrite front page with handlebars

--- backend/src/controllers/home.ts
+++ backend/src/controllers/home.ts
@@ -6,6 +6,24 @@ import { Request, Response } from "express";
  */
 export const index = (req: Request, res: Response) => {
     res.render("home", {
-        title: "home"
+      title: "home",
+      sheets: [
+        {
+          link: "https://drafty.cs.brown.edu/professors",
+          name: "Computer Science Professors",
+          relatedContent: `<p>See an <a href="https://jeffhuang.com/computer_science_professors.html">analysis of computer science hiring trends</a></p>`
+        },
+        {
+          link: "https://drafty.cs.brown.edu/ajobs",
+          name: "Academic Jobs"
+        }
+      ],
+      publications: [
+        {
+          link: "https://jeffhuang.com/Final_Drafty_HCOMP17.pdf",
+          name: "Drafty: Enlisting Users to be Editors who Maintain Structured Data",
+          description: "Shaun Wallace, Lucy van Kleunen, Marianne Aubin-Le Quere, Abraham Peterkin, Yirui Huang, Jeff Huang. HCOMP 2017"
+        },
+      ]
     });
 };
