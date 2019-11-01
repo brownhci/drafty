050a51e5b0f69daad58308cf03b82655399b16cc
pengzhengyi <zhengyi_peng@brown.edu>
2019-10-31 22:00:20 -0400

    update home view

--- backend/src/controllers/home.ts
+++ backend/src/controllers/home.ts
@@ -6,6 +6,6 @@ import { Request, Response } from "express";
  */
 export const index = (req: Request, res: Response) => {
     res.render("home", {
-        title: "Home"
+        title: "home"
     });
 };
