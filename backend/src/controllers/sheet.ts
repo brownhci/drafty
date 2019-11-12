334d46bc3e1d0102bfc826cf35342ddd67f6d589
Shaun Wallace <shaun_wallace@brown.edu>
2019-11-11 21:14:06 -0500

    improve error msg to users + add genSheet to ctrlr

--- backend/src/controllers/sheet.ts
+++ backend/src/controllers/sheet.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import { getRequestedSheetName, hasRequestedSheet } from "../models/sheet";
 import { makeRenderObject } from "../config/handlebars-helpers";
-
+import { genSheets } from "../database/gen_spreadsheets";
 
 /**
  * GET /sheet/:sheet
@@ -10,9 +10,18 @@ import { makeRenderObject } from "../config/handlebars-helpers";
 export function getSheet(req: Request, res: Response) {
   const sheetURL = req.params.sheet;
   if (!hasRequestedSheet(sheetURL)) {
-    req.flash("errors", { msg: "Cannot find requested sheet"});
+    req.flash("errors", { msg: "Oh sorry we cannot find requested sheet :("});
     return res.redirect("/");
   }
   const sheetName = getRequestedSheetName(sheetURL);
   res.render("sheet", makeRenderObject({ title: `Sheet:${sheetName}`, sheetName: sheetName }, req));
+}
+
+/**
+ * POST /sheet/:sheet
+ * Sheet page.
+ */
+export function genSheet(req: Request, res: Response) {
+  genSheets(req.session.user.idProfile);
+  res.status(200).end();
 }
