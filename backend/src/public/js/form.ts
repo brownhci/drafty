b9a1a01c742282e528a8be60a10b7f6057210bc4
pengzhengyi <zhengyi_peng@brown.edu>
2019-11-15 23:14:06 -0500

    move email input into a handlebars partial

--- backend/src/public/js/form.ts
+++ backend/src/public/js/form.ts
@@ -1,11 +1,36 @@
+function removeAssociatedInvalidTooltip(element: HTMLInputElement) {
+  const nextElement = element.nextElementSibling;
+  console.log(nextElement);
+  if (nextElement && nextElement.classList.contains("invalid-tooltip")) {
+    nextElement.remove();
+  }
+}
+
 const formsToValidate = document.getElementsByClassName("custom-validation");
 for (const form of Array.from(formsToValidate)) {
-  const formsToValidate = form as HTMLFormElement;
+  const formToValidate = form as HTMLFormElement;
   form.addEventListener("submit", function(event) {
-    if (formsToValidate.checkValidity() === false) {
+    if (!formToValidate.checkValidity()) {
           event.preventDefault();
           event.stopPropagation();
         }
       form.classList.add("was-validated");
+  });
+
+  const formInputs = form.querySelectorAll("input.form-control");
+  formInputs.forEach(function(formInput) {
+    const formInputToValidate = formInput as HTMLInputElement;
+    formInput.addEventListener("change", function() {
+      const validClass = "is-valid";
+      const invalidClass = "is-invalid";
+      if (formInputToValidate.checkValidity()) {
+        formInput.classList.add(validClass);
+        formInput.classList.remove(invalidClass);
+        removeAssociatedInvalidTooltip(formInputToValidate);
+      } else {
+        formInput.classList.add(invalidClass);
+        formInput.classList.remove(validClass);
+      }
+    });
   });
 }
