31aafdc1d6cef8ddbbf1f29394e9b797b4e53239
Shaun Wallace <shaun_wallace@brown.edu>
2019-12-05 11:51:45 -0500

    Ignore clusterize JS files in TS compilation

--- backend/copyStaticAssets.ts
+++ backend/copyStaticAssets.ts
@@ -1,5 +1,7 @@
 import * as shell from "shelljs";
 
+//shell.cp("-R", "src/public/js/lib", "dist/public/js/"); // from original
+shell.cp("-R", "src/public/js/clusterize/", "dist/public/js/clusterize/");
 shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/webfonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
