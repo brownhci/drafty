1eb62a66c1a94ef2b98da5c0922fa60ea959916b
pengzhengyi <zhengyi_peng@brown.edu>
2019-12-28 17:00:40 +0800

    make small nav list item navigationable

--- backend/src/public/js/navbar.ts
+++ backend/src/public/js/navbar.ts
@@ -1,9 +1,17 @@
 const navbarToggler = document.querySelector("button.navbar-toggler");
-navbarToggler.addEventListener("click", function(event) {
+navbarToggler.addEventListener("click", function() {
   const activeClass = "active";
   if (navbarToggler.classList.contains(activeClass)) {
     navbarToggler.classList.remove(activeClass);
   } else {
     navbarToggler.classList.add(activeClass);
+  }
+});
+const navbarNavSmall = document.querySelector(".navbar-nav-sm");
+navbarNavSmall.addEventListener("click", function(event) {
+  const target = event.target as HTMLElement;
+  const href = target.dataset.href;
+  if (href) {
+    window.location.href = href;
   }
 });
