5299834594ff4be558b921a4c92cb75cd7b15c84
Shaun Wallace <shaun_wallace@brown.edu>
2020-02-15 21:30:23 -0500

    add TODO logic for recording edits

--- backend/src/controllers/interaction.ts
+++ backend/src/controllers/interaction.ts
@@ -18,6 +18,15 @@ export const postEdit = (req: Request, res: Response, next: NextFunction) => {
   // TODO
   const edit = req.body.edit;
   console.log(`received user edit: ${edit}`);
+
+  /*
+  * Each Edit is one of 3 possible scenarios:
+  * 1. The suggested value already exists, therefore we need to increment the confidence
+  * 2. It is a new suggestion for that row/column (idUniqueId/idSuggestionType)
+  * 3. It is an Alias: the edit is similar to an existing suggestion for that row/column (idUniqueId/idSuggestionType)
+  * FINALLY: record the edit happened and return the idSuggestion if #1
+  */
+
   return res.sendStatus(200);
 };
 
