f08dbbe291b7854dba6325b5069b131a5598f266
Shaun Wallace <shaun_wallace@brown.edu>
2020-02-29 22:06:13 -0500

    Update MySQl Procudures and Functions

--- backend/src/database/stored_routines/procedure_new_edit.sql
+++ backend/src/database/stored_routines/procedure_new_edit.sql
@@ -8,52 +8,6 @@
   * FINALLY: record the edit happened and return the idSuggestion if #1
   */
 
-DELIMITER $$
-CREATE PROCEDURE new_suggestion(
-    INOUT  idSuggestion_var INT, 
-    IN  suggestion_var VARCHAR(1000),
-    IN idProfile_var INT
-)
-BEGIN
-    DECLARE sugg_unchanged INT DEFAULT 0;
-    DECLARE sugg_exists INT DEFAULT 0;
-    DECLARE alias_exists INT DEFAULT 0;
-    DECLARE idSuggestionType_var INT;
-    DECLARE idUniqueId_var INT;
-    DECLARE confidence_var INT DEFAULT 1;
-
-START TRANSACTION;  
-    SELECT idSuggestionType INTO idSuggestionType_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
-    SELECT idUniqueId INTO idUniqueId_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
-    SELECT MAX(confidence) + 1 INTO confidence_var FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var;
-
-    SELECT count(*) as ct INTO sugg_unchanged
-    FROM Suggestions WHERE idSuggestion = idSuggestion_var AND suggestion = suggestion_var;
-
-    SELECT count(*) as ct INTO sugg_exists
-    FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;
-    
-    IF sugg_unchanged > 0 THEN
-        /* do nothing */
-        SELECT idSuggestion FROM Suggestions LIMIT 1;
-    ELSEIF alias_exists > 0 THEN
-        /* sw: whoops need to check if alias_exists ;)  */
-        UPDATE Suggestions s SET s.suggestion = suggestion_var WHERE s.idSuggestion = idSuggestion_var; 
-        UPDATE Alias a SET count = count + 1 WHERE a.idSuggestion = idSuggestion_var AND a.alias = suggestion_var;
-    ELSEIF sugg_exists > 0 THEN
-        SELECT idSuggestion INTO idSuggestion_var FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;    
-        UPDATE Suggestions s SET s.confidence = confidence_var WHERE s.idSuggestion = idSuggestion_var;
-    ELSE
-        INSERT INTO Suggestions (idSuggestion, idSuggestionType, idUniqueID, idProfile, suggestion, confidence) VALUES (null, idSuggestionType_var, idUniqueId_var, idProfile_var, suggestion_var, confidence_var);
-        SET idSuggestion_var = (SELECT LAST_INSERT_ID());
-    END IF;
-COMMIT;
-END $$
- 
-DELIMITER ;
-
-
-
 /*
 DELIMITER $$
 CREATE PROCEDURE new_edit(
@@ -102,17 +56,187 @@ DELIMITER ;
 
 /* LOOP THROUGH ROWS https://www.mysqltutorial.org/mysql-cursor/ */
 
+DELIMITER $$
+CREATE FUNCTION get_idUniqueID(idSuggestion_var INT) RETURNS int(11)
+BEGIN
+  DECLARE idUniqueID_rt INT DEFAULT 0;
+  SELECT idUniqueID INTO idUniqueID_rt FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+  RETURN idUniqueID_rt;
+END$$
+DELIMITER ;
 
-/*** INSERT INTERACTION FUNCTION ***/
-DELIMITER //
-CREATE FUNCTION insert_interaction(idSession_var INT, idInteractionType_var INT) RETURNS INT DETERMINISTIC 
+DELIMITER $$
+CREATE FUNCTION get_idSuggestionType(idSuggestion_var INT) RETURNS int(11)
+BEGIN
+  DECLARE idSuggestionType_rt INT DEFAULT 0;
+  SELECT idSuggestionType INTO idSuggestionType_rt FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+  RETURN idSuggestionType_rt;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE new_edit(
+    IN  idSuggestion_var INT, 
+    IN  suggestion_var VARCHAR(1000),
+    IN idProfile_var INT,
+    OUT  idSuggestion_new INT
+)
+BEGIN
+
+    DECLARE sugg_exists INT DEFAULT 0;
+    DECLARE alias_exists INT DEFAULT 0;
+    DECLARE idSuggestionType_var INT;
+    DECLARE idUniqueId_var INT;
+    DECLARE confidence_var INT DEFAULT 1;
+
+START TRANSACTION;
+    SET idSuggestion_new = idSuggestion_var;
+
+    SELECT idSuggestionType INTO idSuggestionType_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+    SELECT idUniqueId INTO idUniqueId_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+    SELECT MAX(confidence) + 1 INTO confidence_var FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var;
+
+    SELECT count(*) as ct INTO sugg_exists
+    FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;
+
+    SELECT count(*) as ct INTO sugg_exists
+    FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;
+ 
+    IF alias_exists > 0 THEN
+        UPDATE Suggestions s SET s.suggestion = suggestion_var WHERE s.idSuggestion = idSuggestion_var; 
+        UPDATE Alias a SET count = count + 1 WHERE a.idSuggestion = idSuggestion_var AND a.alias = suggestion_var;
+    ELSEIF sugg_exists > 0 THEN    
+        UPDATE Suggestions s SET s.confidence = confidence_var WHERE s.idSuggestion = idSuggestion_var;
+    ELSE
+        INSERT INTO Suggestions (idSuggestion, idSuggestionType, idUniqueID, idProfile, suggestion, confidence) VALUES (null, idSuggestionType_var, idUniqueId_var, idProfile_var, suggestion_var, confidence_var);
+        SET idSuggestion_new = (SELECT LAST_INSERT_ID());
+    END IF;
+COMMIT;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE PROCEDURE insert_edit_suggestions(
+    IN idSuggestionPrev_var INT,
+    IN idSuggestionChosen_var INT,
+    IN idSession_var INT,
+    IN idInteractionType_var INT,
+    IN idEntryType_var INT,
+    IN mode_var VARCHAR(25)
+)
+BEGIN
+    DECLARE finished INTEGER DEFAULT 0;
+    DECLARE idEdit_var INTEGER;
+    DECLARE idSugg_var INTEGER;
+    DECLARE isPrevSugg_var INTEGER DEFAULT 0;
+    DECLARE isNewSugg_var INTEGER DEFAULT 0;
+    DECLARE isChosen_var INTEGER DEFAULT 0;
+
+    -- declare cursor for suggestions
+    DEClARE cursorIdSuggs 
+        CURSOR FOR 
+            SELECT idSuggestion FROM Suggestions WHERE idSuggestionType = (SELECT idSuggestionType FROM Suggestions WHERE idSuggestion = idSuggestion_var) AND idUniqueID = (SELECT idUniqueID FROM Suggestions WHERE idSuggestion = idSuggestion_var);
+ 
+    -- declare NOT FOUND handler
+    DECLARE CONTINUE HANDLER 
+        FOR NOT FOUND SET finished = 1;
+
+START TRANSACTION; 
+    -- insert data into Edit and Edit_Suggestions
+    INSERT INTO Edit (IdInteraction, idEntryType, mode) VALUES (insert_interaction(idSession_var,idInteractionType_var), idEntryType_var, mode_var);
+    SET idEdit_var = (SELECT LAST_INSERT_ID());
+
+    SELECT count(*) as ct INTO isNewSugg_var FROM Edit_Suggestions WHERE idSuggestion = idSuggestionChosen_var;
+
+    -- open previous cursor to loop results
+    OPEN cursorIdSuggs;
+ 
+    insertEditSugg: LOOP
+        -- prep loop and check if loop is finished
+        FETCH cursorIdSuggs INTO idSugg_var;
+        IF finished = 1 THEN 
+            LEAVE insertEditSugg;
+        END IF;
+        
+        -- run checks on values
+        IF idSugg_var = idSuggestionPrev_var THEN 
+            SET isPrevSugg_var = 1;
+        ELSE
+            SET isPrevSugg_var = 0;
+        END IF;
+
+        IF idSugg_var = idSuggestionChosen_var THEN 
+            SET isChosen_var = 1;
+            -- check if this was the first time this idSuggested has been edited
+            IF isNewSugg_var > 0 THEN
+                SET isNewSugg_var = 1;
+            END IF;
+        ELSE
+            SET isChosen_var = 0;
+        END IF;
+
+        -- insert new edit edit_suggestion
+        INSERT INTO Edit_Suggestion (idEdit,idSuggestion,isPrevSugg,isNew,isChosen) VALUES (idEdit_var,idSugg_var,isPrevSugg_var,isNewSugg_var,isChosen_var);
+    
+    END LOOP insertEditSugg;
+
+    UPDATE Suggestions SET suggestion = (SELECT username FROM Profile p INNER JOIN Session s ON s.idProfile = p.idProfile WHERE s.idSession = idSession_var) WHERE idSuggestionType = 13 AND idUniqueID = (SELECT idUniqueID FROM Suggestions WHERE idSuggestion = idSuggestionChosen_var);
+    UPDATE Suggestions SET suggestion = CURRENT_TIME WHERE idSuggestionType = 14 AND idUniqueID = (SELECT idUniqueID FROM Suggestions WHERE idSuggestion = idSuggestionChosen_var);
+
+    CLOSE cursorIdSuggs;
+COMMIT;
+END$$
+DELIMITER ;
+
+DELIMITER $$
+CREATE FUNCTION insert_interaction(idSession_var INT, idInteractionType_var INT) RETURNS int(11)
+    DETERMINISTIC
 BEGIN
     DECLARE idInteraction_new INT; 
     INSERT INTO Interaction (idInteraction, idSession, idInteractionType) VALUES (null, idSession_var, idInteractionType_var); 
     SET idInteraction_new = (SELECT LAST_INSERT_ID());
     RETURN idInteraction_new;
-END
+END$$
+DELIMITER ;
 
-//
+DELIMITER $$
+CREATE PROCEDURE new_suggestion(
+    INOUT  idSuggestion_var INT, 
+    IN  suggestion_var VARCHAR(1000),
+    IN idProfile_var INT
+)
+BEGIN
+    DECLARE sugg_unchanged INT DEFAULT 0;
+    DECLARE sugg_exists INT DEFAULT 0;
+    DECLARE alias_exists INT DEFAULT 0;
+    DECLARE idSuggestionType_var INT;
+    DECLARE idUniqueId_var INT;
+    DECLARE confidence_var INT DEFAULT 1;
 
+START TRANSACTION;  
+    SELECT idSuggestionType INTO idSuggestionType_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+    SELECT idUniqueId INTO idUniqueId_var FROM Suggestions WHERE idSuggestion = idSuggestion_var;
+    SELECT MAX(confidence) + 1 INTO confidence_var FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var;
+
+    SELECT count(*) as ct INTO sugg_unchanged
+    FROM Suggestions WHERE idSuggestion = idSuggestion_var AND suggestion = suggestion_var;
+
+    SELECT count(*) as ct INTO sugg_exists
+    FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;
+    
+    IF sugg_unchanged > 0 THEN
+        /* do nothing */
+        SELECT idSuggestion FROM Suggestions LIMIT 1;
+    ELSEIF alias_exists > 0 THEN
+        UPDATE Suggestions s SET s.suggestion = suggestion_var WHERE s.idSuggestion = idSuggestion_var; 
+        UPDATE Alias a SET count = count + 1 WHERE a.idSuggestion = idSuggestion_var AND a.alias = suggestion_var;
+    ELSEIF sugg_exists > 0 THEN
+        SELECT idSuggestion INTO idSuggestion_var FROM Suggestions WHERE idSuggestionType = idSuggestionType_var AND idUniqueId = idUniqueId_var AND suggestion = suggestion_var;    
+        UPDATE Suggestions s SET s.confidence = confidence_var WHERE s.idSuggestion = idSuggestion_var;
+    ELSE
+        INSERT INTO Suggestions (idSuggestion, idSuggestionType, idUniqueID, idProfile, suggestion, confidence) VALUES (null, idSuggestionType_var, idUniqueId_var, idProfile_var, suggestion_var, confidence_var);
+        SET idSuggestion_var = (SELECT LAST_INSERT_ID());
+    END IF;
+COMMIT;
+END$$
 DELIMITER ;
