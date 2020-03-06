d55c12b7735658bfa3ef5deee6a01b665b9d35a3
Shaun Wallace <shaun_wallace@brown.edu>
2020-03-06 12:58:59 -0500

    only pull active rows

--- util/build_spreadsheet.py
+++ util/build_spreadsheet.py
@@ -87,10 +87,11 @@ def build_table_data_section(rows_iter, nrows_in_section=NROWS_IN_SECTION):
 
 def build_table_data_sections(cursor):
     sql = '''
-SELECT s.idSuggestion, s.idSuggestionType, s.idUniqueID, s.suggestion, st.columnOrder
-            FROM Suggestions s
-            INNER JOIN SuggestionType st ON st.idSuggestionType = s.idSuggestionType
-            WHERE s.active = 1 AND st.isActive = 1 AND idUniqueID > 0
+            SELECT s.idSuggestion, s.idSuggestionType, s.idUniqueID, s.suggestion, st.columnOrder 
+            FROM Suggestions s 
+            INNER JOIN SuggestionType st ON st.idSuggestionType = s.idSuggestionType 
+            INNER JOIN UniqueId u ON u.idUniqueID = s.idUniqueID 
+            WHERE s.active = 1 AND st.isActive = 1 AND u.active = 1
             ORDER BY idUniqueID, st.columnOrder, confidence desc
           '''
     cursor.execute(sql)
