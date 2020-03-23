70098e3b7c54e99184f54fab3edd074fd5f54cea
Shaun Wallace <shaun_wallace@brown.edu>
2020-03-23 15:32:08 -0400

    copy env file to dist

--- backend/copyStaticAssets.ts
+++ backend/copyStaticAssets.ts
@@ -4,3 +4,4 @@ shell.cp("-R", "src/public/js/fuse/", "dist/public/js/");
 shell.cp("-R", "src/public/fonts", "dist/public/");
 shell.cp("-R", "src/public/webfonts", "dist/public/");
 shell.cp("-R", "src/public/images", "dist/public/");
+shell.cp("-R", ".env", "dist/");
