8d17136ef572ed62412fbd6c499e2e39a105fa62
Shaun Wallace <shaun_wallace@brown.edu>
2020-03-31 16:09:00 -0400

    attempt1 to control user browser zoom

--- backend/src/public/js/sheet.ts
+++ backend/src/public/js/sheet.ts
@@ -1,6 +1,14 @@
 // TODO ARROW KEY not functioning when scrolling off screen
 // TODO add new row
 
+/* Force screen zoom to 100% - otherwise cell input form will flash when selecting the first few rows */
+// https://stackoverflow.com/questions/1713771/how-to-detect-page-zoom-level-in-all-modern-browsers/5078596#5078596
+let scale: string = 'scale(1)';
+document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+//document.body.style.msTransform     =   scale;   // IE 9 :: sw getting a TS error saying this doesn't exist
+document.body.style.transform       = scale;     // General
+document.body.style.zoom = "1"; // 100%
+
 /* which table column is active: a table column is activated when associated head is clicked */
 let activeTableColElement: null | HTMLTableColElement = null;
 
