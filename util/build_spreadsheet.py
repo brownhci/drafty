b845fd459df8734a4f1766d9b0a902ac32f11827
Shaun Wallace <shaun_wallace@brown.edu>
2020-04-12 20:39:03 -0400

    fix bug in isFreeEdit

--- util/build_spreadsheet.py
+++ util/build_spreadsheet.py
@@ -61,7 +61,7 @@ def build_column_label_cell(row, column_index):
     else:
         noneditable_indices.append(column_index)
         content_editable = 'contenteditable="false"'
-    free_edit = 'true' if row['isFreeEdit'] else 'false'
+    free_edit = 'false' if row['isFreeEdit'] else 'true'
     return f'<th id="column-label{column_index}" data-id-suggestion-type="{id_suggestion_type}" data-autocomplete-only="{free_edit}" tabindex="-1" class="column-label" {content_editable}>{colname}<button class="sort-btn"></button></th>\n'
 
 
