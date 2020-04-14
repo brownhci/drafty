f2e1ee0af47d58feca6fd59f7d37842f62437e41
Shaun Wallace <shaun_wallace@brown.edu>
2020-04-14 15:02:19 -0400

    re-order sheet access logic

--- backend/src/controllers/sheet.ts
+++ backend/src/controllers/sheet.ts
@@ -9,13 +9,13 @@ import { makeRenderObject } from "../config/handlebars-helpers";
  */
 export function getSheet(req: Request, res: Response) {
   const sheetURL = req.params.sheet;
-  if(!req.user) {
-    res.render("account/signup", makeRenderObject({ title: "Signup" }, req));
-  } else if (!hasRequestedSheet(sheetURL)) {
+  if (!hasRequestedSheet(sheetURL)) {
     if(sheetURL !== "service-worker.js") { // sw bug: service-worker.js is getting this endpoint
       req.flash("errors", { msg: "Oh sorry we cannot find requested sheet :("});
     }
     return res.redirect("/");
+  } else if(!req.user) {
+    res.render("account/signup", makeRenderObject({ title: "Signup" }, req));
   } else {
     const sheetName = getRequestedSheetName(sheetURL);
     const sheetPath = getRequestedSheetPath(sheetURL);
