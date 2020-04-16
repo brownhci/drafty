85ac899b434395055b0d1c68e328127c5762320e
Shaun Wallace <shaun_wallace@brown.edu>
2020-04-16 10:03:13 -0400

    update db migration

--- backend/src/database/migrations/disable_usa_canada_univ_names.sql
+++ backend/src/database/migrations/disable_usa_canada_univ_names.sql
@@ -1,15 +1,41 @@
 UPDATE SuggestionTypeValues 
 SET active = 0
-WHERE value LIKE '% - USA%';
+WHERE value LIKE '% - USA%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
 
 UPDATE SuggestionTypeValues 
 SET active = 0
-WHERE value LIKE '% - CANADA%';
+WHERE value LIKE '% - CANADA%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
 
 UPDATE SuggestionTypeValues 
 SET active = 0
-WHERE value LIKE '% (CANADA)%';
+WHERE value LIKE '% (CANADA)%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
 
 UPDATE SuggestionTypeValues 
 SET active = 0
-WHERE value LIKE '% (USA)%';
+WHERE value LIKE '% (USA)%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
+
+---
+
+UPDATE SuggestionTypeValues 
+SET value = REPLACE(value, " - USA", "")
+WHERE value LIKE '% - USA%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
+
+UPDATE SuggestionTypeValues 
+SET value = REPLACE(value, " - CANADA", "")
+WHERE value LIKE '% - CANADA%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
+
+UPDATE SuggestionTypeValues 
+SET value = REPLACE(value, " (CANADA)", "")
+WHERE value LIKE '% (CANADA)%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
+
+UPDATE SuggestionTypeValues 
+SET value = REPLACE(value, " (USA)", "")
+WHERE value LIKE '% (USA)%'
+AND (idSuggestionType = 3 OR idSuggestionType = 4 OR idSuggestionType = 5);
