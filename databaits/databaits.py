f3e126fc530c8b119e5aa694369656e8569f8cde
dianalee22 <na_kyoung_lee@brown.edu>
2020-04-23 23:30:58 +0900

    worked on first four databaits

--- databaits/databaits.py
+++ databaits/databaits.py
@@ -2,13 +2,15 @@ import sys
 import argparse
 import pandas as pd 
 import matplotlib.pyplot as plt
+import datetime
 
 def get_data(file_location, index):
     return pd.read_csv(file_location, index_col = index)
 
 def databait_1(df, column, label, time_column, time_type):
     """
-    For DataBait 1
+    For DataBait 1: 
+    > The total number of [label in column] grew [stat]% in the past [time range].
     Given a column and a label to count, creates a distribution over time.
 
     :params:
@@ -20,47 +22,79 @@ def databait_1(df, column, label, time_column, time_type):
     :returns:
     :years: a distribution of those categories over time
     """
-    # NOTE: below is for when there are multiple columns =============
-    #
-    # error checking inputs
-    # num_cols = len(columns)
-    # if num_cols > 2:
-    #     raise ValueError("You can only analyze up to 2 columns")
-    # if len(categories) != num_cols:
-    #     raise ValueError("You need to enter one category for each column")
-    
-    # criteria = zip(columns, categories)
-    # df = pd.read_csv("data/professors.csv", index_col = "UniqueId")
-
-    # filter by criteria inputted
-    # for col, cat in criteria:f
-    #     df = df.loc[df[col] == cat]
-    # pd.set_option('display.max_rows', None)
-    #
-    # ===============================================================
     df = df.loc[df[column] == label]
-
     years = df[time_column].dropna().astype('int32')
     years.hist()
 
     counts = years.value_counts(sort=False).sort_index(ascending=False)
-    print(counts)
-    most_recent_yr = counts.index[0]
+    now = datetime.datetime.now()
 
-    # TODO: code once we decide how to choose year range
-    return counts
+    # TODO: write cases for other types of time
+    best_range = 0
+    best_rate = 0
+
+    if time_type == "Year":
+        year = now.year
+        earliest_year = counts.index[-1]
+        # NOTE: is the past 15-20 years still interesting? should we limit it to 5-10?
+        # (or other more recent ranges)
+        for i in range(5, min(20, year - earliest_year), 5):
+            new = counts.loc[counts.index >= year - i].sum()
+            old = counts.loc[counts.index < year - i].sum()
+            if new/old > best_rate:
+                best_rate = new/old
+                best_range = i
+    print(best_range, best_rate)
+    return best_range, best_rate
 
 def databait_2(df, column, label1, label2, time_column, time_type):
     """
     For DataBait 2
+    > [Label 1 in column] grew [#]% more than [label 2 in same column] 
+    > in the past [time range]. 
     Does the same thing as for Databait 1, but for two labels and 
     produces a comparison between their rates of growth
     """
-    dist1 = databait_1(df, column, label1, time_column, time_type)
-    dist2 = databait_1(df, column, label2, time_column, time_type)
-    # TODO: code once we decide how to choose year range
+    df1 = df.loc[df[column] == label1]
+    years1 = df1[time_column].dropna().astype('int32')
+    counts1 = years1.value_counts(sort=False).sort_index(ascending=False)
+    
+    df2 = df.loc[df[column] == label2]
+    years2 = df2[time_column].dropna().astype('int32')
+    counts2 = years2.value_counts(sort=False).sort_index(ascending=False)
+    now = datetime.datetime.now()
 
-    return None
+    # TODO: write cases for other types of time
+    best_range = 0
+    best_rate = 0
+    bigger = None
+    smaller = None
+
+    if time_type == "Year":
+        year = now.year
+        earliest_year = max(counts1.index[-1], counts2.index[-1])
+        for i in range(5, min(20, year - earliest_year), 5):
+            # NOTE: Can we put this logic into a helper function? 
+            new1 = counts1.loc[counts1.index >= year - i].sum()
+            old1 = counts1.loc[counts1.index < year - i].sum()
+            roc1 = new1 / old1
+            
+            new2 = counts2.loc[counts2.index >= year - i].sum()
+            old2 = counts2.loc[counts2.index < year - i].sum()
+            roc2 = new2 / old2
+            diff = (max(roc1, roc2) - min(roc1, roc2)) / min(roc1, roc2)
+            if  diff > best_rate:
+                best_rate = diff
+                best_range = i
+                if roc1 > roc2: 
+                    bigger = label1
+                    smaller = label2
+                else:
+                    bigger = label2
+                    smaller = label1
+    
+    print(best_range, best_rate, bigger, smaller)
+    return best_range, best_rate, bigger, smaller
 
 def databait_3(df, column1, label1, column2, label2, time_column, time_type):
     """
@@ -70,11 +104,30 @@ def databait_3(df, column1, label1, column2, label2, time_column, time_type):
     big_df = df.loc[df[column1] == label1]
     small_df = big_df.loc[df[column2] == label2]
 
-    years = df[time_column].dropna().astype('int32')
+    years = small_df[time_column].dropna().astype('int32')
     counts = years.value_counts(sort=False).sort_index(ascending=False)
+    print(counts)
+    # TODO: implement error checking in case there is no such data
+    now = datetime.datetime.now()
 
-    # TODO: code once we decide how to choose year range
-    return years
+    # TODO: write cases for other types of time
+    best_range = 0
+    best_rate = 0
+
+    if time_type == "Year":
+        year = now.year
+        earliest_year = counts.index[-1]
+        # NOTE: is the past 15-20 years still interesting? should we limit it to 5-10?
+        # (or other more recent ranges) + sidenote: it's interesting if the ROC is really small
+        for i in range(5, min(25, year - earliest_year), 5):
+            new = counts.loc[counts.index >= year - i].sum()
+            old = counts.loc[counts.index < year - i].sum()
+            print(new, old)
+            if new/old > best_rate:
+                best_rate = new/old
+                best_range = i
+    print(best_range, best_rate)
+    return best_range, best_rate
 
 def databait_4(df):
     # TODO: code once we decide how to choose year range
@@ -107,7 +160,6 @@ def databait_6(df, column, time_column, time_point):
     :max_label: most frequently occuring label
     :ratio: frequency of max_label
     """
-    # TODO: decide whether to do time point/range
     time_df = df.loc[df[time_column] == time_point]
     labels = time_df[column].dropna()
     counts = labels.value_counts(sort=False)
@@ -140,9 +192,11 @@ if __name__ == '__main__':
                         help='the index column for the pandas dataframe')
     args = parser.parse_args()
     df = get_data(args.csv,args.index)
-    print(databait_1(df, "University", "Brown University", "JoinYear", "Year"))
+    # databait_1(df, "University", "Brown University", "JoinYear", "Year")
+    # databait_2(df, "University", "Carnegie Mellon University", "Cornell University", "JoinYear", "Year")
+    databait_3(df, "University", "Arizona State University", "SubField", "Security & Privacy", "JoinYear", "Year")
 
     # databait_1(df, "University", "Brown University", "JoinYear", "Year")
     # print(databait_5(df, "University", "JoinYear", "Year", "2016"))
-    print(databait_8(df, "University", "Bachelors"))
+    # print(databait_8(df, "University", "Bachelors"))
 
