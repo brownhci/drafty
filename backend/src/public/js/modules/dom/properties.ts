88b608fcc05f4a5a9b2a45d8f3804660502d7f28
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-04 18:01:57 -0400

    add a module containing utility functions for retrieving properties (DOM and JS) from HTMLElement

---
+++ backend/src/public/js/modules/dom/properties.ts
@@ -0,0 +1,28 @@
+/**
+ * @module This module encapsulates utility function to manipulate **property** of any HTML element.
+ *
+ * A property is one of the following
+ *    + a HTML attribute, regulated by functions like {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute}
+ *    + a property defined on HTMLElement {@link https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement} of the specific subclass of HTMLElement.
+ *    + a custom property
+ */
+
+export function getProperty(element: HTMLElement, propertyName: string) {
+  if (element.hasAttribute(propertyName)) {
+    return element.getAttribute(propertyName);
+  } else {
+    return element[propertyName];
+  }
+}
+
+export function setProperty(element: HTMLElement, propertyName: string, propertyValue: any) {
+  if (element.hasAttribute(propertyName)) {
+    element.setAttribute(propertyName, propertyValue);
+  } else {
+    element[propertyName] = propertyValue;
+  }
+}
+
+export function hasProperty(element: HTMLElement, propertyName: string) {
+  return element.hasAttribute(propertyName) || propertyName in element;
+}
