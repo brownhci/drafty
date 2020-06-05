a11a5b3b1d8016200d7e49d90a1e46c9ab5802b8
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-05 00:01:34 -0400

    add first unit test and correct error

--- backend/src/public/js/modules/utils/Set.ts
+++ backend/src/public/js/modules/utils/Set.ts
@@ -1,4 +1,9 @@
 /**
+ * @module
+ * This module provides utility functions for {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set Set}
+ */
+
+/**
  * Determines whether first set is subset of second set
  *
  *    A set is considered a subset of another set if all of its elements are present in the second set
@@ -9,9 +14,9 @@
  */
 export function isSubset(setA: Set<any>, setB: Set<any>): boolean {
   for (const setAElement of setA) {
-    if (setB.has(setAElement)) {
-      return true;
+    if (!setB.has(setAElement)) {
+      return false;
     }
   }
-  return false;
+  return true;
 }
