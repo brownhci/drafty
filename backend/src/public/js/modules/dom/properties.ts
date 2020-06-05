e71d1759b6e0f91480ee1d85f7cad552101b83bc
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-05 01:34:09 -0400

    add test for properties module

--- backend/src/public/js/modules/dom/properties.ts
+++ backend/src/public/js/modules/dom/properties.ts
@@ -1,5 +1,6 @@
 /**
- * @module This module encapsulates utility function to manipulate **property** of any HTML element.
+ * @module
+ * This module encapsulates utility function to manipulate **property** of any HTML element.
  *
  * A property is one of the following
  *    + a HTML attribute, regulated by functions like {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/getAttribute}
@@ -11,18 +12,18 @@ export function getProperty(element: HTMLElement, propertyName: string) {
   if (element.hasAttribute(propertyName)) {
     return element.getAttribute(propertyName);
   } else {
-    return element[propertyName];
+    return (element as { [key: string]: any })[propertyName];
   }
+}
+
+export function hasProperty(element: HTMLElement, propertyName: string) {
+  return element.hasAttribute(propertyName) || propertyName in element;
 }
 
 export function setProperty(element: HTMLElement, propertyName: string, propertyValue: any) {
   if (element.hasAttribute(propertyName)) {
     element.setAttribute(propertyName, propertyValue);
   } else {
-    element[propertyName] = propertyValue;
+    (element as { [key: string]: any })[propertyName] = propertyValue;
   }
-}
-
-export function hasProperty(element: HTMLElement, propertyName: string) {
-  return element.hasAttribute(propertyName) || propertyName in element;
 }
