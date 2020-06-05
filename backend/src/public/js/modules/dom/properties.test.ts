fdf0248498b3dbef0327c531e71420acdb5cf24b
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-05 03:27:30 -0400

    modify properties test to increase coverage

--- backend/src/public/js/modules/dom/properties.test.ts
+++ backend/src/public/js/modules/dom/properties.test.ts
@@ -2,11 +2,11 @@ import { getProperty, hasProperty, setProperty } from "./properties";
 
 let element: HTMLElement;
 
-beforeEach(() => {
-  element = document.createElement("div");
-});
-
 describe("DOM property", () => {
+  beforeEach(() => {
+    element = document.createElement("div");
+  });
+
   test("non-existing property", () => {
     expect(getProperty(element, "contenteditable")).toBeFalsy();
     expect(hasProperty(element, "contenteditable")).toBe(false);
@@ -20,6 +20,7 @@ describe("DOM property", () => {
 
   test("setting a DOM property", () => {
     expect(hasProperty(element, "class")).toBe(false);
+    element.className = "";
     setProperty(element, "class", "active");
     expect(getProperty(element, "class")).toBe("active");
     expect(hasProperty(element, "class")).toBe(true);
@@ -27,6 +28,10 @@ describe("DOM property", () => {
 });
 
 describe("JS property", () => {
+  beforeEach(() => {
+    element = document.createElement("div");
+  });
+
   test("has JS property", () => {
     expect(hasProperty(element, "textContent")).toBe(true);
     expect(hasProperty(document.createElement("tr"), "cells")).toBe(true);
@@ -41,6 +46,10 @@ describe("JS property", () => {
 });
 
 describe("custom property", () => {
+  beforeEach(() => {
+    element = document.createElement("div");
+  });
+
   const customPropertyName = "isTestDiv";
   test("has custom property", () => {
     expect(hasProperty(element, customPropertyName)).toBe(false);
