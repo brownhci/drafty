494cbf61e6f5ce208053464b03bf6f2f8e7c966f
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-05 18:26:46 -0400

    update Abstraction documentation and refine Instantiation.ts to only keep a core ForwardingInstantiation

--- backend/src/public/js/modules/components/sheet/table-data-manager/Abstraction.ts
+++ backend/src/public/js/modules/components/sheet/table-data-manager/Abstraction.ts
@@ -5,7 +5,6 @@
  *
  * These properties, like regular properties defined on an object, can be accessed and modified using regular dot syntax or bracket syntax. However, their access/modification is channeled through property descriptor (more specifically, access descriptors which are getter and setter functions) {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/getOwnPropertyDescriptor}, which can facilitate advanced mechanisms like proxy or aliasing.
  * Read {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get getter} and {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set setter} for inspiration.
- *
  */
 
 import { NotImplemented } from "../../../utils/errors";
@@ -15,27 +14,51 @@ import { isSubset } from "../../../utils/Set";
  * The key name of a property.
  *
  * @see {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty}
- * @typedef {(string | number | symbol)} Prop
  */
 export type Prop = string | number | symbol;
 
 /**
+ * An abstraction groups together a set of properties.
+ *
  * These core functionalities are exposed by the Abstraction class:
- *    + @protected access to registered properties {@link Abstraction#propNames_}
- *    + @public iterate through property name, value pair {@link Abstraction#[Symbol.iterator]}
- *    + @public registering/revoking properties {@link Abstraction#registerProps__}
- *    + @public detecting whether another object (might be or might noe be an Abstraction) has same properties registered {@link Abstraction#hasSameShape__}
+ * 
+ *    + @protected {@link Abstraction#propNames_} access to registered properties
+ *    + @public {@link Abstraction#[Symbol.iterator]} iterate through property name, value pair
+ *    + @public {@link Abstraction#registerProps__} registering/revoking properties
+ *    + @public {@link Abstraction#hasSameShape__} detecting whether another object (might be or might noe be an Abstraction) has same properties registered
  *
  * These functionalities can be overriden:
- *    + @protected create a descriptor for a property given part of a complete property descriptor (usually just the getter and setter) {@link Abstraction.createDescriptor__}
+ *    + @protected {@link Abstraction.createDescriptor__} create a descriptor for a property given part of a complete property descriptor (usually just the getter and setter)
  *
  * Its subclasses should follow these naming conventions:
+ * 
  *    + registered properties should be intact (unmodified) and can be accessed directly
  *      @example Suppose `foo` is a registered property, it should be accessible from `this.foo`
  *    + implementation-specific variable will be preceded by one underscores if it is private, otherwise, it will be suffixed by one underscore
  *    + implementation-specific method (except methods like constructor or Symbol.iterator required by class) will be preceded by two underscores if it is private, otherwise, it will be suffixed by two underscores
  *
- * @classdesc An abstraction groups together a set of properties.
+ * Its subclass fields should follow these ordering convention:
+ * 
+ *    1. type definitions
+ *    2. static class attributes {@link #ordering-principle}
+ *    3. instance attributes {@link #ordering-principle}
+ *    4. constructor
+ *    5. overriden Object methods like Symbol.iterator, toString...
+ *    6. class static methods {@link #ordering-principle}
+ *    7. instance methods {@link #ordering-principle}
+ *
+ * ###### Ordering Principle
+ * 
+ *     + grouped by relevance
+ *     + ordered
+ *       1. first by calling hierarchy
+ *          1. callee
+ *          2. caller
+ *       2. then by access privilege
+ *          1. public
+ *          2. protected
+ *          3. private
+ *       3. exception can be made if facilitates understanding
  */
 export abstract class Abstraction {
   protected propNames_: Set<Prop>;
@@ -43,8 +66,11 @@ export abstract class Abstraction {
   /**
    * Creates an Abstraction instance.
    *
-   * @param {Record<Prop, Partial<PropertyDescriptor>} props: An object containing mapping from properties to their descriptors.
+   * @param {Record<Prop, Partial<PropertyDescriptor>} props - An object containing mapping from properties to their descriptors.
    * @constructs Abstraction
+   *
+   * @example
+   *    If you want to create an Abstraction and add properties later, you can invoke constructor with `{}`: `new Abstraction({});`
    */
   constructor(props: Record<Prop, Partial<PropertyDescriptor>>) {
     this.registerProps__(props);
@@ -64,6 +90,56 @@ export abstract class Abstraction {
   }
 
   /**
+   * Creates a default descriptor that
+   * 
+   *    + is configurable (so that it can be replaced or revoked in {@link Abstraction#registerProps__}
+   *    + is enumerable
+   *    + will report NotImplemented error when uses [[Get]] or [[Set]] to access the property
+   *
+   * @param {Prop} property - Name of property.
+   * @return {PropertyDescriptor} A descriptor that can be used as argument in {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty Object.defineProperty()}.
+   * @throws {NotImplemented} When getter or setter is not provided but access/modification is performed.
+   */
+  private static __defaultDescriptor(property: Prop): PropertyDescriptor {
+    return {
+        configurable: true,
+        enumerable: true,
+        get(): any {
+          throw new NotImplemented(`Getter for ${property.toString()} has not been implemented`);
+        },
+        set(newValue: any) {
+          throw new NotImplemented(`Setter for ${property.toString()} has not been implemented: received ${newValue}`);
+        }
+      };
+  }
+
+  /**
+   * Create a descriptor by overriding default descriptor in {@link Abstraction.__defaultDescriptor}.
+   *
+   * Notes:
+   * 
+   *    + Passing null / undefined to descriptor will result in a clone of default descriptor
+   *
+   * @param {Prop} property - Name of property.
+   * @param {Partial<PropertyDescriptor>} descriptor - An object containing overrides to default descriptor.
+   * @return {PropertyDescriptor} A descriptor that can be used as argument in {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty Object.defineProperty()}.
+   */
+  protected static createDescriptor__(property: Prop, descriptor: Partial<PropertyDescriptor>): PropertyDescriptor {
+    return Object.assign({}, this.__defaultDescriptor(property), descriptor);
+  }
+
+  /**
+   * Registers properties on current instance.
+   *
+   * @param {Record<Prop, Partial<PropertyDescriptor>>} props - An object whose keys represent the names of properties to be defined or modified and whose values are objects describing those properties. Each value in props must provide a descriptor or contain overrides to default descriptor.
+   */
+  private __setPropertyDescriptors(props: Record<Prop, Partial<PropertyDescriptor>>) {
+    for (const [property, descriptor] of Object.entries(props)) {
+      Object.defineProperty(this, property, Abstraction.createDescriptor__(property, descriptor));
+    }
+  }
+
+  /**
    * Registers properties into current abstraction.
    *
    * All registered properties will have *proxied* access in that even though they are bound to the current instance and can be accessed using the dot syntax, their access and modification are regulated by {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty access descriptor}.
@@ -71,6 +147,7 @@ export abstract class Abstraction {
    * You can think PropertyDescriptor as a Proxy that defines how access and modification to an attribute is eventually resolved. And the Abstraction has these props as pseudo-properties whose access/modification are proxied.
    *
    * This method can be used to
+   * 
    *    + register properties to a fresh new instance
    *    + add properties to a instance with existing properties registered
    *    + remove all existing registered properties from an instance
@@ -114,6 +191,7 @@ export abstract class Abstraction {
    * Another instance has the same **shape** with current instance when it has same properties registered.
    *
    * Note:
+   * 
    *    + property does not need to be registered as an own property in the other instance, rather it can be a property registered in the prototype chain.
    *
    * @public
@@ -121,7 +199,6 @@ export abstract class Abstraction {
    * @return {boolean} True if two instances have same shape.
    *
    * @example
-   * // @return {boolean} true
    *    Suppose current instance has `foo`, `bar` registered as properties, then it has same shape with
    *    `{foo: 'some foo value', bar: 'some bar value'}
    *    or
@@ -133,60 +210,12 @@ export abstract class Abstraction {
     } else {
       // treat other as an object
       for (const propName of this.propNames_) {
-        // use the {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in in} operator to look through the prototype chain
+        /** use the {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/in in} operator to look through the prototype chain*/ 
         if (!(propName in other)) {
           return false;
         }
       }
       return true;
-    }
-  }
-
-  /**
-   * Creates a default descriptor that
-   *    + is configurable (so that it can be replaced or revoked in {@link Abstraction#registerProps__}
-   *    + is enumerable
-   *    + will report NotImplemented error when uses [[Get]] or [[Set]] to access the property
-   *
-   * @param {Prop} property - Name of property.
-   * @return {PropertyDescriptor} A descriptor that can be used as argument in {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty Object.defineProperty()}.
-   * @throws {NotImplemented} When getter or setter is not provided but access/modification is performed.
-   */
-  private static __defaultDescriptor(property: Prop): PropertyDescriptor {
-    return {
-        configurable: true,
-        enumerable: true,
-        get(): any {
-          throw new NotImplemented(`Getter for ${property.toString()} has not been implemented`);
-        },
-        set(newValue: any) {
-          throw new NotImplemented(`Setter for ${property.toString()} has not been implemented: received ${newValue}`);
-        }
-      };
-  }
-
-  /**
-   * Create a descriptor by overriding default descriptor in {@link Abstraction.__defaultDescriptor}.
-   *
-   * Notes:
-   *    + Passing null / undefined to descriptor will result in a clone of default descriptor
-   *
-   * @param {Prop} property - Name of property.
-   * @param {Partial<PropertyDescriptor>} descriptor - An object containing overrides to default descriptor.
-   * @return {PropertyDescriptor} A descriptor that can be used as argument in {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty Object.defineProperty()}.
-   */
-  protected static createDescriptor__(property: Prop, descriptor: Partial<PropertyDescriptor>): PropertyDescriptor {
-    return Object.assign({}, this.__defaultDescriptor(property), descriptor);
-  }
-
-  /**
-   * Registers properties on current instance.
-   *
-   * @param {Record<Prop, Partial<PropertyDescriptor>>} props - An object whose keys represent the names of properties to be defined or modified and whose values are objects describing those properties. Each value in props must provide a descriptor or contain overrides to default descriptor.
-   */
-  private __setPropertyDescriptors(props: Record<Prop, Partial<PropertyDescriptor>>) {
-    for (const [property, descriptor] of Object.entries(props)) {
-      Object.defineProperty(this, property, Abstraction.createDescriptor__(property, descriptor));
     }
   }
 }
--- backend/src/public/js/modules/components/sheet/table-data-manager/Instantiation.ts
+++ backend/src/public/js/modules/components/sheet/table-data-manager/Instantiation.ts
@@ -1,141 +1,120 @@
 import { Abstraction, Prop } from "./Abstraction";
 
+/**
+ * Strips the getter-setter pair of functions from PropertyDescriptor so that access functions type annotations can be overiden.
+ *
+ * @see {@link https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_es5_d_.propertydescriptor.html PropertyDescriptor} {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperty}
+ */
+type DataDescriptor = Omit<PropertyDescriptor, "get" | "set">;
 
-export type Translator = (key: Prop) => Prop;
-type TranslationTable = Map<Prop, Prop>;
-interface TranslationTablePair {
-  forward: TranslationTable;
-  backward: TranslationTable;
+/**
+ * Extracts the getter-setter pair of functions form PropertyDescriptor.
+ *
+ *    PropertyDescriptor = AccessFunctions + DataDescriptor
+ */
+type AccessFunctions = Pick<PropertyDescriptor, "get" | "set">;
+
+/**
+ * Adds the access functions back into the {@link DataDescriptor} but with different annotations: the argument list will end with `forwardingTo` which contains the forwarding target and `thisArgument` which contains the `ForwardingInstantiation` instance.
+ */
+export interface ForwardingPropertyDescriptor extends DataDescriptor {
+  get: (forwardingTo: any, thisArgument: ForwardingInstantiation) => any;
+  set: (v: any, forwardingTo: any, thisArgument: ForwardingInstantiation) => void;
 }
 
+/**
+ * A ForwardingInstantiation forwards access/modification operations to an underlying object.
+ *
+ * These core functionalities are exposed by the ForwardingInstantiation class:
+ * 
+ *    + @public {@link ForwardingInstantiation#setForwardingTo__} change the forwarding target
+ *    + @public {@link ForwardingInstantiation#registerProps__} registering/revoking properties
+ *
+ * @augments Abstraction
+ */
 export class ForwardingInstantiation extends Abstraction {
-  private forwardingTo: any;
+  private _forwardingTo: any;
 
-  private translationTablePair: TranslationTablePair;
-
-  constructor(properties: Record<Prop, Translator>, globalTranslator: Translator = undefined, forwardingTo: any) {
-    /** superProps will be registered in superclass */
-    const superProps: Record<Prop, Partial<PropertyDescriptor>> = {};
-
-    for (const propertyName in properties) {
-      superProps[propertyName] = {
-        get: () => {
-          return this.forwardingTo[this.__translateForward(propertyName)];
-        },
-        set: (newValue: any) => {
-          this.forwardingTo[this.__translateForward(propertyName)] = newValue;
-        }
-      };
-    }
-
-    super(superProps);
-
-    this.setForwardingTo(forwardingTo);
-
-    Object.defineProperty(this, "translationTablePair", {
-        configurable: false,
-        enumerable: false,
-        value: ForwardingInstantiation.__buildTranslationTable(properties, globalTranslator),
-        writable: false
-    });
+  /**
+   * Creates an Abstraction instance.
+   *
+   * @param {Record<Prop, Partial<ForwardingPropertyDescriptor>} props: An object containing mapping from properties to their descriptors.
+   * @param {any} setForwardingTo - A target to which access/modification operations are forwarded.
+   * @constructs ForwardingInstantiation
+   *
+   * @example
+   *    If you want to create a ForwardingInstantiation and set forwarding target/add properties later, you can invoke constructor like: `new ForwardingInstantiation({}, null);`
+   */
+  constructor(propsToForward: Record<Prop, Partial<ForwardingPropertyDescriptor>>, forwardingTo: any) {
+    super({});
+    this.setForwardingTo__(forwardingTo);
+    this.registerProps__(propsToForward, false);
   }
 
-  protected setForwardingTo(forwardingTo: any) {
-    Object.defineProperty(this, "forwardingTo", {
+  /**
+   * Transform a ForwardingPropertyDescriptor into a PropertyDescriptor. More specifically, it creates wrapper getter and setter that invoke the ForwardingPropertyDescriptor's getter and setter by supplying additional arguments through scoping.
+   *
+   * @param {Partial<ForwardingPropertyDescriptor>} descriptor - An object containing partial implementation of a ForwardingPropertyDescriptor.
+   * @param {ForwardingInstantiation} thisArgument - The invoking context: an ForwardingInstantiation which provides a forwarding target.
+   * @return {Partial<PropertyDescriptor>} A partial implementation of a property descriptor.
+   */
+  private static __transformPropertyDescriptor(descriptor: Partial<ForwardingPropertyDescriptor>, thisArgument: ForwardingInstantiation): Partial<PropertyDescriptor> {
+    const accessFunctions: AccessFunctions = {
+      get: () => {
+        return descriptor.get(thisArgument._forwardingTo, thisArgument);
+      },
+      set: (v: any) => {
+        return descriptor.set(v, thisArgument._forwardingTo, thisArgument);
+      }
+    };
+    return Object.assign({}, descriptor, accessFunctions);
+  }
+
+  /**
+   * Transforms all ForwardingPropertyDescriptor into PropertyDescriptor and remaps them under same properties.
+   *
+   * @see {@link ForwardingInstantiation.__transformPropertyDescriptor}
+   * @param {Record<Prop, Partial<ForwardingPropertyDescriptor>>} props - A mapping from property name to property descriptor (ForwardingPropertyDescriptor).
+   * @param {ForwardingInstantiation} thisArgument - The invoking context: an ForwardingInstantiation which provides a forwarding target.
+   * @return {Record<Prop, Partial<PropertyDescriptor>>} A mapping from property name to property descriptor (PropertyDescriptor)
+   */
+  private static __transformPropertyDescriptors(props: Record<Prop, Partial<ForwardingPropertyDescriptor>>, thisArgument: ForwardingInstantiation): Record<Prop, Partial<PropertyDescriptor>> {
+    const _props: Record<Prop, Partial<PropertyDescriptor>> = {};
+    for (const [property, descriptor] of Object.entries(props)) {
+      _props[property] = this.__transformPropertyDescriptor(descriptor, thisArgument);
+    }
+    return _props;
+  }
+
+  /**
+   * @override
+   * @public
+   * @param {Record<Prop, Partial<ForwardingInstantiation>>} props - An object contains mapping from Prop to ForwardingPropertyDescriptor.
+   * @param {boolean} [reset=false] - Whether existing props will be removed.
+   * @description __override__ The overriding function allows access functions in ForwardingPropertyDescriptor to receive two additional arguments: `forwardingTo` and `thisArgument`.
+   */
+  registerProps__(props: Record<Prop, Partial<ForwardingPropertyDescriptor>>, reset: boolean = false) {
+    /** props will be registered in {@link ./Abstraction.Abstraction} */
+    super.registerProps__(ForwardingInstantiation.__transformPropertyDescriptors(props, this), reset);
+  }
+
+  /**
+   * Set a target to which access and modification on registered properties will be forwarded.
+   *
+   * In essence, the current instance serves as a {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Proxy Proxy} which forwards all operations involving the registered properties to the underlying target.
+   *
+   * @example
+   *    Suppose the registered property is `id` and the `forwardingTo` is an object. Then `this.id` is equivalent to `forwardingTo.id`.
+   *
+   * @public
+   * @param {any} forwardingTo - A target to forward access / modification on regiserted properties.
+   */
+  setForwardingTo__(forwardingTo: any) {
+    Object.defineProperty(this, "_forwardingTo", {
         configurable: false,
         enumerable: false,
         value: forwardingTo,
         writable: true
     });
   }
-
-  private __translateForward(propertyName: Prop): Prop {
-    return this.translationTablePair.forward.get(propertyName);
-  }
-
-  private __translateBackward(propertyName: Prop): Prop {
-    return this.translationTablePair.backward.get(propertyName);
-  }
-
-  private static __buildTranslationTable(properties: Record<Prop, Translator>, globalTranslator: Translator = undefined): TranslationTablePair {
-    const forwardTranslationTable = new Map();
-    const backwardTranslationTable = new Map();
-
-    for (const [property, translator] of Object.entries(properties)) {
-      let translatedProperty: Prop;
-      if (translator) {
-        translatedProperty = translator(property);
-      } else if (globalTranslator) {
-        translatedProperty = globalTranslator(property);
-      } else {
-        translatedProperty = property;
-      }
-
-      forwardTranslationTable.set(property, translatedProperty);
-      backwardTranslationTable.set(translatedProperty, property);
-    }
-
-    return {
-      forward: forwardTranslationTable,
-      backward: backwardTranslationTable
-    };
-  }
 }
-
-
-export class ProxyInstantiation extends ForwardingInstantiation {
-
-  constructor(properties: Record<string, any>) {
-    /** superProps will be registered in superclass */
-    const superProps: Record<string, Translator> = {};
-    /** props will be registered in this class */
-    const props: Record<string, PropertyDescriptor> = {};
-
-    for (const propertyName in properties) {
-      superProps[propertyName] = undefined;
-
-      props[ProxyInstantiation.__concretizeKey(propertyName)] = {
-        configurable: false,
-        enumerable: false,
-        value: properties[propertyName],
-        writable: true
-      };
-    }
-
-    super(superProps, ProxyInstantiation.__concretizeKey, undefined);
-    this.setForwardingTo(this);
-    Object.defineProperties(this, props);
-  }
-
-  /**
-   * For each property passed to constructor, a pair of getter and setter is used to create a pseudo-property. The actual **concrete** property will be registered under the transformed key name. In other words, this pair of getter and setter becomes a proxy to access and modify the **concrete** property.
-   *
-   * @example
-   *    Suppose `foo` is passed as a property, both methods below can access and modify the property:
-   *      + the **concrete** property can be accessed and modified via `this[this.constructor.__concretizeKey('foo')]`
-   *      + `this.foo` or `this['foo']` invokes the getter or setter of the pseudo-property, which is proxied to access or modify the **concrete** property
-   *
-   * @private
-   * @static
-   * @param {string} pseudoKey - The key under which getter and setter for this property are registered.
-   * @return {string} The real **concrete** under which the property value is stored as a class field.
-   */
-  private static __concretizeKey(pseudoKey: string): string {
-    return `$${pseudoKey}`;
-  }
-
-  /**
-   * The reverse operation of {@see __concretizeKey}. Converting from a **concrete** property to the **virtual** property (used by getter and setter)
-   *
-   * @private
-   * @static
-   * @param {string} concreteKey - The real **concrete** under which the property value is stored as a class field.
-   *
-   * @return {string} The pseudo key under which getter and setter for this property are registered.
-   */
-  private static __virtualizeKey(concreteKey: string): string {
-    return concreteKey.slice(1);
-  }
-}
-
-export { Prop };
