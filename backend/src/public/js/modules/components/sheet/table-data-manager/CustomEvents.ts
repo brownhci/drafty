a93f9f04eeb6863863c0e2e9af3a6330cd140c70
pengzhengyi <zhengyi_peng@brown.edu>
2020-06-07 02:17:31 -0400

    provide documentation for custom events

--- backend/src/public/js/modules/components/sheet/table-data-manager/CustomEvents.ts
+++ backend/src/public/js/modules/components/sheet/table-data-manager/CustomEvents.ts
@@ -1,10 +1,55 @@
-import { ViewModel } from "./ViewModel";
+/**
+ * @module
+ *
+ * This modules defines a set of custom events {@link https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent} and their additional `detail`.
+ *
+ * These events can be emitted from the {@link MutationReporter/MutationReporter#report} method when an observed target receives corresponding type of mutations.
+ *
+ *
+ * | Mutation (Event Name) | Event                    | Event Detail                   | Triggering Example       |
+ * | --------------------- | ------------------------ | ------------------------------ | ------------------------ |
+ * | propertyChange        | PropertyChangeEvent      | PropertyChangeEventDetail      | el.id = "<newID>"        |
+ * | characterDataChange   | CharacterDataChangeEvent | CharacterDataChangeEventDetail | el.textContent = "..."   |
+ * | childListChange       | ChildListChangeEvent     | ChildListChangeEventDetail     | el.appendChild(newChild) |
+ *
+ * Similar as DOM events, these custom events
+ *
+ *    + can be listened by event listeners {@link https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener}
+ *    + have propagation phase, target phase, and bubbling phase {@link https://www.w3.org/TR/DOM-Level-3-Events/#event-flow}
+ *
+ * Different from DOM events, these custom events
+ *
+ *    + have a `detail` field which contains data describing the triggering mutation.
+ */
 
+
+/**
+ * Interface describing the `detail` field of a {@link PropertyChangeEvent}
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord}
+ */
 export interface PropertyChangeEventDetail {
+  /** the local name of the changed attribute */
   attributeName: string;
+  /** the value of the changed attribute before the change, `null` if current mutation initializes the attribute */
   oldAttributeValue: string;
 }
 
+/**
+ * A custom event corresponds to a attribute mutation.
+ *
+ * Note:
+ *
+ *   + Attribute is associated with the element rather than the JS variable.
+ *     @example `class` is a valid attribute while `classList` is not
+ *   + An attribute mutation might be trigger from
+ *     + user's interaction with the HTML page
+ *     + direct manipulation of attribute through JS {@link https://developer.mozilla.org/en-US/docs/Web/API/Element/setAttribute}
+ *     + manipulation of related JS property
+ *       @example adding a class through `element.classList.add` will trigger an attribute mutation on the `class` attribute
+ *
+ * @augments CustomEvent
+ */
 export class PropertyChangeEvent extends CustomEvent<PropertyChangeEventDetail> {
   static readonly typeArg: string = "propertyChange";
 
@@ -17,13 +62,37 @@ export class PropertyChangeEvent extends CustomEvent<PropertyChangeEventDetail> 
   }
 }
 
+
+/**
+ * Interface describing the `detail` field of a {@link CharacterDataChangeEvent}
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord}
+ */
 export interface CharacterDataChangeEventDetail {
+  /** the text content of the involved element before the mutation */
   oldValue: string;
+  /** the text content of the involved element after the mutation */
   newValue: string;
 }
 
-export type CharacterDataChangeDetail = Omit<CharacterDataChangeEventDetail, "targetViewModel">;
-
+/**
+ * A custom event corresponds to a text content mutation.
+ *
+ * Note:
+ *
+ *   + A text content mutation might be triggered by
+ *     + addition of text node
+ *       @example `appendChild`...
+ *     + removal of text node
+ *       @example `removeChild`...
+ *     + replacement of text node
+ *       @example `replaceChild`, `innerHTML`, `innerText`, `textContent`...
+ *     + modification of text node
+ *       @example `nodeValue`, user inputting...
+ *   + A text content mutation might happen together with a childList mutation, for example, as the result of `innerHTML`. The criterion for whether a childList mutation qualifies as a text content mutation is whether there is any text node added or removed.
+ *
+ * @augments CustomEvent
+ */
 export class CharacterDataChangeEvent extends CustomEvent<CharacterDataChangeEventDetail> {
   static readonly typeArg: string = "characterDataChange";
 
@@ -36,13 +105,28 @@ export class CharacterDataChangeEvent extends CustomEvent<CharacterDataChangeEve
   }
 }
 
+
+/**
+ * Interface describing the `detail` field of a {@link ChildListChangeEvent}
+ *
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/MutationRecord}
+ */
 export interface ChildListChangeEventDetail {
+  /** The nodes added. Will be an empty NodeList if no nodes were added. */
   addedNodes: NodeList;
+  /** The nodes removed. Will be an empty NodeList if no nodes were removed. */
   removedNodes: NodeList;
+  /** The previous sibling of the added or removed nodes, or null. */
   previousSibling: Node;
+  /** The next sibling of the added or removed nodes, or null. */
   nextSibling: Node;
 }
 
+/**
+ * A custom event corresponds to a child list mutation.
+ *
+ * @augments CustomEvent
+ */
 export class ChildListChangeEvent extends CustomEvent<ChildListChangeEventDetail> {
   static readonly typeArg: string = "childListChange";
 
