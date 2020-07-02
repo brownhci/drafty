be858019538026502fdaf4849018b2ede1ac2004
pengzhengyi <zhengyi_peng@brown.edu>
2020-07-01 21:26:32 -0400

    add comment for debounce function

--- backend/src/public/js/modules/utils/debounce.ts
+++ backend/src/public/js/modules/utils/debounce.ts
@@ -3,8 +3,12 @@ type EventHandler = (event: Event) => void;
 
 /**
  *
- * @function debounce
  * Debounces a callback function using {@link https://developer.mozilla.org/en-US/docs/Web/API/window/requestAnimationFrame Window.requestAnimationFrame}
+ * 
+ * @this is resolved as in an arrow function
+ *
+ * @function debounce
+ * @param {EventHandler} callback - The callback to be debounced.
  */
 export const debounce = (callback: EventHandler): EventHandler => {
   let ticking = false;
