cfd6753f1f37c87724e1e3face86b8a5c58bd94e
pengzhengyi <zhengyi_peng@brown.edu>
2020-07-24 21:43:38 -0400

    regulate contextmenu buttons under menuitem class, add a insert row button at column label contextmenu

--- backend/src/public/js/modules/components/sheet/contextmenu.ts
+++ backend/src/public/js/modules/components/sheet/contextmenu.ts
@@ -6,29 +6,9 @@ import { isButton } from "../../dom/types";
 import { activeTableCellElement, activateTableCol } from "../../../sheet";
 
 
-
 const tableDataContextMenu: HTMLElement = document.getElementById("table-data-contextmenu");
 const columnLabelContextMenu: HTMLElement = document.getElementById("column-label-contextmenu");
 const contextMenuClass = "contextmenu";
-
-export const insertRowMenuItem: HTMLButtonElement = document.getElementById("menuitem-insert-row") as HTMLButtonElement;
-export const countMenuItem: HTMLButtonElement = document.getElementById("menuitem-count") as HTMLButtonElement;
-
-function getMenuItem(element: HTMLElement): HTMLButtonElement {
-  return element.closest("button") as HTMLButtonElement;
-}
-function getMenuItemAction(menuItem: HTMLButtonElement): string {
-  return menuItem.innerText;
-}
-function deactivateMenuItem(menuItem: HTMLButtonElement) {
-  menuItem.classList.remove(activeClass);
-}
-function toggleMenuItemActiveState(menuItem: HTMLButtonElement) {
-  menuItem.classList.toggle(activeClass);
-}
-export function isContextMenuButton(element: HTMLElement) {
-  return isButton(element) && element.parentElement.classList.contains(contextMenuClass);
-}
 export function activateTableDataContextMenu(event: MouseEvent) {
   tableDataContextMenu.classList.add(activeClass);
   placeElementInViewport(tableDataContextMenu, event.clientX, event.clientY);
@@ -44,10 +24,123 @@ export function deactivateColumnLabelContextMenu() {
   columnLabelContextMenu.classList.remove(activeClass);
 }
 
+class MenuItem {
+  static readonly buttonToMenuItem: Map<HTMLButtonElement, MenuItem> = new Map();
+
+  readonly item: HTMLButtonElement;
+  /**
+   * if current menu item is activated, aliased menu items will be activated
+   * if current menu item is deactivated, aliased menu item will also be deactivated
+   */
+  readonly alias: Set<MenuItem> = new Set();
+  /** if current menu item is activated, conflicted menu items will be deactivated <*/
+  readonly conflicts: Set<MenuItem> = new Set();
+
+  get action(): string {
+    return this.item.innerText;
+  }
+
+  constructor(item: HTMLButtonElement, alias: Iterable<MenuItem> = [], conflicts: Iterable<MenuItem> = []) {
+    this.item = item;
+    for (const menuItem of alias) {
+      this.alias.add(menuItem);
+    }
+    for (const menuItem of conflicts) {
+      this.conflicts.add(menuItem);
+    }
+
+    MenuItem.buttonToMenuItem.set(item, this);
+  }
+
+  static find(element: HTMLElement): MenuItem {
+    if (!element) {
+      return null;
+    }
+    if (!isButton(element)) {
+      element = element.closest("button");
+    }
+
+    return this.buttonToMenuItem.get(element as HTMLButtonElement);
+  }
+
+  activate() {
+    this.item.classList.add(activeClass);
+    for (const menuItem of this.alias) {
+      menuItem.item.classList.add(activeClass);
+    }
+    for (const menuItem of this.conflicts) {
+      menuItem.item.classList.remove(activeClass);
+    }
+  }
+
+  deactivate() {
+    this.item.classList.remove(activeClass);
+    for (const menuItem of this.alias) {
+      menuItem.item.classList.remove(activeClass);
+    }
+  }
+
+  toggle() {
+    if (this.item.classList.contains(activeClass)) {
+      this.deactivate();
+    } else {
+      this.activate();
+    }
+  }
+
+  /**
+   * Flags a menu item as current menu item's alias.
+   *
+   * @param {MenuItem} menuItem - The menu item that will be in current menu item's alias.
+   * @param {boolean} [mutual = true] - Whether this alias relationship is mutual. In other words, whether current men item will be in provided menu item's alias.
+   */
+  addToAlias(menuItem: MenuItem, mutual: boolean = true) {
+    this.alias.add(menuItem);
+    if (mutual) {
+      menuItem.alias.add(this);
+    }
+  }
+
+  /**
+   * Flags a menu item as current menu item's conflicts.
+   *
+   * @param {MenuItem} menuItem - The menu item that will be in current menu item's conflicts.
+   * @param {boolean} [mutual = true] - Whether this conflicts relationship is mutual. In other words, whether current men item will be in provided menu item's conflicts.
+   */
+  addToConflicts(menuItem: MenuItem, mutual: boolean = true) {
+    this.conflicts.add(menuItem);
+    if (mutual) {
+      menuItem.conflicts.add(this);
+    }
+  }
+}
+
+// menu items
+export const columnLabelButtons = columnLabelContextMenu.children;
+export const columnLabelCopyMenuItem = new MenuItem(columnLabelButtons[0] as HTMLButtonElement);
+export const columnLabelCopyColumnMenuItem = new MenuItem(columnLabelButtons[1] as HTMLButtonElement);
+export const columnLabelInsertRowMenuItem = new MenuItem(columnLabelButtons[2] as HTMLButtonElement);
+export const columnLabelCountMenuItem = new MenuItem(columnLabelButtons[3] as HTMLButtonElement);
+export const columnLabelDistributionMenuItem = new MenuItem(columnLabelButtons[4] as HTMLButtonElement);
+
+export const tableDataButtons = tableDataContextMenu.children;
+export const tableDataCopyMenuItem = new MenuItem(tableDataButtons[0] as HTMLButtonElement);
+export const tableDataPasteMenuItem = new MenuItem(tableDataButtons[1] as HTMLButtonElement);
+export const tableDataInsertRowMenuItem = new MenuItem(tableDataButtons[2] as HTMLButtonElement);
+
+/* set up alias and conflicts */
+columnLabelInsertRowMenuItem.addToAlias(tableDataInsertRowMenuItem);
+columnLabelInsertRowMenuItem.addToConflicts(columnLabelCountMenuItem);
+tableDataInsertRowMenuItem.addToConflicts(columnLabelCountMenuItem);
+
+export function isContextMenuButton(element: HTMLElement) {
+  return isButton(element) && element.parentElement.classList.contains(contextMenuClass);
+}
+
 // event handler
 tableDataContextMenu.addEventListener("click", function(event: MouseEvent) {
-  const menuItem = getMenuItem(event.target as HTMLElement);
-  switch (getMenuItemAction(menuItem)) {
+  const menuItem = MenuItem.find(event.target as HTMLElement);
+  switch (menuItem.action) {
     case "Copy":
       copyTableCellElement(activeTableCellElement);
       break;
@@ -55,8 +148,7 @@ tableDataContextMenu.addEventListener("click", function(event: MouseEvent) {
       pasteToTableCellElement(activeTableCellElement);
       break;
     case "Insert row":
-      deactivateMenuItem(countMenuItem);
-      toggleMenuItemActiveState(menuItem);
+      menuItem.toggle();
       toggleInsertion();
       break;
   }
@@ -64,17 +156,20 @@ tableDataContextMenu.addEventListener("click", function(event: MouseEvent) {
   event.stopPropagation();
 }, true);
 columnLabelContextMenu.addEventListener("click", function(event: MouseEvent) {
-  const menuItem = getMenuItem(event.target as HTMLElement);
-  switch (getMenuItemAction(menuItem)) {
+  const menuItem = MenuItem.find(event.target as HTMLElement);
+  switch (menuItem.action) {
     case "Copy column":
       activateTableCol();
       // fallthrough
     case "Copy":
       copyTableCellElement(activeTableCellElement);
       break;
+    case "Insert row":
+      menuItem.toggle();
+      toggleInsertion();
+      break;
     case "Count":
-      deactivateMenuItem(insertRowMenuItem);
-      toggleMenuItemActiveState(menuItem);
+      menuItem.toggle();
       toggleRowCount();
       break;
     case "Distribution":
