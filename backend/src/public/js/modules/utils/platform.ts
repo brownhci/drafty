74cc0c35e5acf256b1cb1702f6f2c0e2ee711e8d
pengzhengyi <zhengyi_peng@brown.edu>
2020-07-27 18:05:09 -0400

    add firefox patch for locating cell editor

--- backend/src/public/js/modules/utils/platform.ts
+++ backend/src/public/js/modules/utils/platform.ts
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 // platform
 /**
  * Tells whether the browser runs on a Mac.
@@ -8,4 +10,10 @@ function isMac() {
   const platform = window.navigator.platform;
   return platform.includes("Mac");
 }
+
+/** {@link https://stackoverflow.com/questions/9847580/how-to-detect-safari-chrome-ie-firefox-and-opera-browser} */
+// Firefox 1.0+
+// @ts-ignore
+export const isFirefox = typeof InstallTrigger !== 'undefined';
+
 export const onMac: boolean = isMac();
