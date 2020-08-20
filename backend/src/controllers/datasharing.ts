9a4403c2fd35180335831cf2581f8415fbc553e5
Shaun Wallace <shaun_wallace@brown.edu>
2020-08-20 13:03:26 -0400

    fix camel case error

--- backend/src/controllers/datasharing.ts
+++ backend/src/controllers/datasharing.ts
@@ -34,18 +34,3 @@ export const getCSV = (req: Request, res: Response) => {
     return res.sendStatus(500);
   }
 };
-
-/**
- * GET /data/csv
- * Help page.
- */
-export const getCSV_old = (req: Request, res: Response) => {
-  if(req.session.user.isAuth) {
-    console.log(req.originalUrl);
-    const fileName = req.originalUrl.substring(req.originalUrl.lastIndexOf("/") + 1);
-    const file = path.join(__dirname, "../../data_sharing/" + fileName + ".csv");
-    res.download(file); // Set disposition and send it.
-  } else {
-    return res.sendStatus(500);
-  }
-};
