187532d7f84a8c756f817fcf72aa1790c7dff5bd
Shaun Wallace <shaun_wallace@brown.edu>
2020-08-24 19:27:27 -0400

    fix procedure

--- backend/src/database/stored_routines/procedure_new_row.sql
+++ backend/src/database/stored_routines/procedure_new_row.sql
@@ -18,7 +18,6 @@ CREATE PROCEDURE new_suggestion_new_row(
     OUT idSuggestion_new INT
 )
 BEGIN
-    DECLARE idSuggestion_var INT;
     DECLARE confidence_var INT DEFAULT 1;
 
 START TRANSACTION;  
