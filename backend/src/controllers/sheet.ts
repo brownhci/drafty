b9078ec22f2e59fdb6e75b14e2a5272d44b4c1e3
Shaun Wallace <shaun_wallace@brown.edu>
2020-09-01 13:17:59 -0400

    make grabbing sheets async

--- backend/src/controllers/sheet.ts
+++ backend/src/controllers/sheet.ts
@@ -7,7 +7,7 @@ import { makeRenderObject } from "../config/handlebars-helpers";
  * GET /sheet/:sheet
  * Sheet page.
  */
-export function getSheet(req: Request, res: Response) {
+export async function getSheet(req: Request, res: Response) {
   const sheetURL = req.params.sheet;
   if (!hasRequestedSheet(sheetURL)) {
     if(sheetURL !== "service-worker.js") { // sw bug: service-worker.js is getting this endpoint
@@ -17,8 +17,8 @@ export function getSheet(req: Request, res: Response) {
   } else if(!req.user) {
     res.render("account/signup", makeRenderObject({ title: "Signup" }, req));
   } else {
-    const sheetName = getRequestedSheetName(sheetURL);
-    const sheetPath = getRequestedSheetPath(sheetURL);
+    const sheetName = await getRequestedSheetName(sheetURL);
+    const sheetPath = await getRequestedSheetPath(sheetURL);
     res.render("pages/sheet", makeRenderObject({ title: `Sheet:${sheetName}`, sheetName: sheetName, sheetPath: sheetPath }, req));
   }
 }
