2ea3e1a47314c6725a2e9fd7d1d673220596dcac
Shaun Wallace <shaun_wallace@brown.edu>
2020-09-02 14:35:43 -0400

    sheets on homepage pulled from sheets yaml config

--- backend/src/controllers/home.ts
+++ backend/src/controllers/home.ts
@@ -1,32 +1,41 @@
 import { Request, Response } from "express";
 import { makeRenderObject } from "../config/handlebars-helpers";
+import { sheetsData }  from "../models/sheet";
+
+
+function getSheets() {
+  let sheets = new Array();
+  console.log(' -- ',Object.keys(sheetsData));
+
+  sheetsData.forEach((data, sheetUrl) => {
+    if(data.on_homepage) {
+      const link = '/' + sheetUrl
+      sheets.push({
+        link: link,
+        name: data.name
+      });
+    }
+  })
+  return sheets
+}
 
 /**
  * GET /
  * Home page.
  */
 export const index = (req: Request, res: Response) => {
-    res.render("pages/home", makeRenderObject({
-      ignoreHeader: true,
-      ignoreFooter: true,
-      title: "Home",
-      sheets: [
-        {
-          link: "/professors",
-          name: "Computer Science Professors",
-          relatedContent: `<p>See an <a href="https://jeffhuang.com/computer_science_professors.html">analysis of computer science hiring trends</a></p>`
-        },
-        {
-          link: "/ajobs",
-          name: "Academic Jobs"
-        }
-      ],
-      publications: [
-        {
-          link: "https://jeffhuang.com/Final_Drafty_HCOMP17.pdf",
-          name: "Drafty: Enlisting Users to be Editors who Maintain Structured Data",
-          description: "Shaun Wallace, Lucy van Kleunen, Marianne Aubin-Le Quere, Abraham Peterkin, Yirui Huang, Jeff Huang. HCOMP 2017"
-        },
-      ]
-    }, req));
+  console.log('getSheets() = ',getSheets());
+  res.render("pages/home", makeRenderObject({
+    ignoreHeader: true,
+    ignoreFooter: true,
+    title: "Home",
+    sheets: getSheets(),
+    publications: [
+      {
+        link: "https://jeffhuang.com/Final_Drafty_HCOMP17.pdf",
+        name: "Drafty: Enlisting Users to be Editors who Maintain Structured Data",
+        description: "Shaun Wallace, Lucy van Kleunen, Marianne Aubin-Le Quere, Abraham Peterkin, Yirui Huang, Jeff Huang. HCOMP 2017"
+      },
+    ]
+  }, req));
 };
