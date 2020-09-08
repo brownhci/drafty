18b242235fd2832f55d906bd036b06ba5111eb8c
Shaun Wallace <shaun_wallace@brown.edu>
2020-09-08 11:03:23 -0400

    improve error reporting

--- backend/src/controllers/errors.ts
+++ backend/src/controllers/errors.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { makeRenderObject } from "../config/handlebars-helpers";
 import { sheetsData }  from "../models/sheet";
 import path from "path";
+import logger from "../util/logger";
 
 function getSheets() {
     const sheets: Array<Record<string, any>> = [];
@@ -21,19 +22,21 @@ function getSheets() {
  * GET /
  * Home page after 500 error.
  */
-export async function error500 (req: Request, res: Response) {
-    res.render(path.join(__dirname, "../../views/pages/home500"), makeRenderObject({
-      errors: true,
-      ignoreHeader: true,
-      ignoreFooter: true,
-      title: "Home",
-      sheets: getSheets(),
-      publications: [
-        {
-          link: "https://jeffhuang.com/Final_Drafty_HCOMP17.pdf",
-          name: "Drafty: Enlisting Users to be Editors who Maintain Structured Data",
-          description: "Shaun Wallace, Lucy van Kleunen, Marianne Aubin-Le Quere, Abraham Peterkin, Yirui Huang, Jeff Huang. HCOMP 2017"
-        },
-      ]
-    }, req));
-  }
+export async function error500 (err: Error, req: Request, res: Response) {
+  console.log('ERROR --- error500(): ');
+  logger.error(err);
+  res.render(path.join(__dirname, "../../views/pages/home500"), makeRenderObject({
+    errors: true,
+    ignoreHeader: true,
+    ignoreFooter: true,
+    title: "Home",
+    sheets: getSheets(),
+    publications: [
+      {
+        link: "https://jeffhuang.com/Final_Drafty_HCOMP17.pdf",
+        name: "Drafty: Enlisting Users to be Editors who Maintain Structured Data",
+        description: "Shaun Wallace, Lucy van Kleunen, Marianne Aubin-Le Quere, Abraham Peterkin, Yirui Huang, Jeff Huang. HCOMP 2017"
+      },
+    ]
+  }, req));
+}
