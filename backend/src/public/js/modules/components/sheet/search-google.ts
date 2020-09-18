b0b43dd941b6065e3281daf8a4dff63e498e31b1
Shaun Wallace <shaun_wallace@brown.edu>
2020-09-18 12:12:47 -0400

    create client side handler for search google

---
+++ backend/src/public/js/modules/components/sheet/search-google.ts
@@ -0,0 +1,23 @@
+import { getEnclosingTableRow } from "../../dom/navigate";
+import { recordGoogleSearch } from "../../api/record-interactions";
+
+// sw: TODO - mark in DB which columns someone should search by
+function buildGoogleSearchURL(tableRow: HTMLTableRowElement) {
+    // https://www.google.com/search?hl=en&q=jeff+huang+brown+university
+    let url: string = "https://www.google.com/search?hl=en&q=";
+    for(const cell of tableRow.cells) {
+        url += cell.textContent.replace(/\s+/g,"+") + "+";
+    }
+    return url;
+}
+
+export function openGoogleSearch(tableCellElement: HTMLTableCellElement) {
+    //console.log("Search Google",tableCellElement);
+    const tableRow: HTMLTableRowElement = getEnclosingTableRow(tableCellElement);
+    const url: string = buildGoogleSearchURL(tableRow);
+    window.open(url);
+    // record in DB
+    const idSuggestion = tableCellElement.id;
+    const idRow = tableRow.getAttribute("data-id");
+    recordGoogleSearch(idSuggestion,idRow,tableRow);
+}
