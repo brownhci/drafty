a5ce1c0a9ae3faed83d57b3989b73b3e4774246f
Shaun Wallace <shaun_wallace@brown.edu>
2020-09-23 17:33:32 -0400

    add click and double click

--- user_interest_profile/main.py
+++ user_interest_profile/main.py
@@ -10,6 +10,24 @@ sql_col_order = """
                 SELECT * FROM SuggestionType st WHERE isActive = 1 ORDER BY st.columnOrder
                 """
 
+sql_click = """
+SELECT i.timestamp, i.idInteractionType, it.interaction, i.idSession, ses.idProfile, c.*
+FROM Interaction i
+INNER JOIN users.Session ses ON ses.idSession = i.idSession
+INNER JOIN InteractionType it ON it.idInteractionType = i.idInteractionType
+INNER JOIN Click c on i.idInteraction = c.idInteraction
+WHERE ses.idProfile = %s;
+"""
+
+sql_double_click = """
+SELECT i.timestamp, i.idInteractionType, it.interaction, i.idSession, ses.idProfile, dc.*
+FROM Interaction i
+INNER JOIN users.Session ses ON ses.idSession = i.idSession
+INNER JOIN InteractionType it ON it.idInteractionType = i.idInteractionType
+INNER JOIN DoubleClick dc on i.idInteraction = dc.idInteraction
+WHERE ses.idProfile = %s;
+"""
+
 def get_db_creds():
     dbuser, dbpass = 'test', 'test'
     with open('../backend/.env', 'r') as fh:
@@ -25,15 +43,30 @@ def get_db_creds():
     return dbuser, dbpass
 
 def column_order_test(cursor):
-    with db.cursor() as cursor:
-        cursor.execute(sql_col_order)
-        rows = cursor.fetchall()
-        print(rows,'\n')
-        for r in rows:
-            print(r['idSuggestionType'],r['name'])
+    cursor.execute(sql_col_order)
+    rows = cursor.fetchall()
+    print(rows,'\n')
+    for r in rows:
+        print(r['idSuggestionType'],r['name'])
+
+def click_test(cursor):
+    example_idProfile = 66430
+    cursor.execute(sql_click, (example_idProfile))
+    rows = cursor.fetchall()
+    #print(rows,'\n')
+    for r in rows:
+        print(r)
+
+def double_click_test(cursor):
+    example_idProfile = 66430
+    cursor.execute(sql_double_click, (example_idProfile))
+    rows = cursor.fetchall()
+    #print(rows,'\n')
+    for r in rows:
+        print(r)
 
 if __name__ == '__main__':
-    # python3 main.py --host localhost --database csprofessors
+    # python3 main.py --host 128.148.36.16 --database csprofessors
     parser = argparse.ArgumentParser(description='Write database data to table HTML file.')
     parser.add_argument('--host', default='localhost',
                         help='The host of the MySQL database')
@@ -50,6 +83,7 @@ if __name__ == '__main__':
     try:
         with db.cursor() as cursor:
             column_order_test(cursor)
+            click_test(cursor)
     except Exception as e:
         print('ERROR exiting...')
         print(e)
