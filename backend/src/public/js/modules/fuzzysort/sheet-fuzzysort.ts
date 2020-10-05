329c0093b91abee9806a4df8917353e2542c9de3
Shaun Wallace <shaun_wallace@brown.edu>
2020-10-05 10:03:55 -0400

    add code for replacing fuse's fuzzy search algo

---
+++ backend/src/public/js/modules/fuzzysort/sheet-fuzzysort.ts
@@ -0,0 +1,6 @@
+import Fuzzysort from "./fuzzysort";
+import { Option as Opt } from "../components/sheet/suggestions";
+import { fuseSelectRootContainerClass, autocompleteSuggestionClass, previousEditClass, optionContainerClass, optionClass, optionTextClass } from "../constants/css-classes";
+import { activeClass } from "../constants/css-classes";
+import { executeAtLeisure } from "../utils/defer";
+
