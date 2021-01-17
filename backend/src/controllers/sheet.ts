e1dcbc7fbbb40dab7b4581fd8fa4f7e4efb7d834
Shaun Wallace <shaun_wallace@brown.edu>
2021-01-17 16:20:04 -0500

    add get sheet history to controller

--- backend/src/controllers/sheet.ts
+++ backend/src/controllers/sheet.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { sheetsData, getRequestedSheetName, getRequestedSheetPath } from "../models/sheet";
+import { sheetsData, getRequestedSheetName, getRequestedSheetPath, getRequestedEditHistorySheetPath } from "../models/sheet";
 import { makeRenderObject } from "../config/handlebars-helpers";
 // import { genSheets } from "../database/gen_spreadsheets";
 
@@ -30,5 +30,31 @@ export async function getSheet(req: Request, res: Response) {
     const sheetName = await getRequestedSheetName(sheetURL);
     const sheetPath = await getRequestedSheetPath(sheetURL);
     res.render("pages/sheet", makeRenderObject({ title: `Sheet: ${sheetName}`, sheetName: sheetName, sheetPath: sheetPath }, req));
+  }
+}
+
+/**
+ * GET /:sheet/edit_history
+ * Sheet page.
+ */
+export async function getSheetEditHistory(req: Request, res: Response) {
+  const sheetURL = req.params.sheet;
+  
+  // check if sheet exists
+  if (!sheetsData.has(sheetURL)) {
+    return res.redirect("/");
+  } 
+
+  // run through permissions/settings
+  const sheetData = sheetsData.get(sheetURL);
+
+  if(!req.session.user.isAuth && sheetData.login_required) {
+    res.render("account/signup", makeRenderObject({ title: "Signup" }, req));
+  } else if (req.session.user.isAuth && !req.session.user.isAdmin && sheetData.admin_only) {
+    req.flash("errors", { msg: "Oh sorry this sheet is protected or under development :("});
+  } else {
+    const sheetName = await getRequestedSheetName(sheetURL);
+    const editHistoryPath = await getRequestedEditHistorySheetPath(sheetURL);
+    res.render("pages/edit_history", makeRenderObject({ title: `Sheet: ${sheetName}`, sheetName: sheetName, editHistoryPath: editHistoryPath }, req));
   }
 }
