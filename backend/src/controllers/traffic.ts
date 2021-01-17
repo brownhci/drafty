e163e4122f38fcb9c3af37a3ab0453e6f7fd7f70
Shaun Wallace <shaun_wallace@brown.edu>
2021-01-17 16:38:39 -0500

    add edit_history to traffic loging

--- backend/src/controllers/traffic.ts
+++ backend/src/controllers/traffic.ts
@@ -3,25 +3,25 @@ import { v4 as uuidv4 } from "uuid";
 import { insertTraffic } from "../database/traffic";
 import path from "path";
 
-const trackedViews = ["","csopenrankings","csprofessors","account","login","signup","help"];
+const trackedViews = ["","csopenrankings","csprofessors","edit_history","account","login","signup","help"];
 
 /**
  * GLOBAL MIDDLEWARE
  */
 export const trafficLogger = (req: Request, res: Response, next: NextFunction) => {
-    const url  = path.basename(req.url);
-    
-    if (trackedViews.includes(url)) {
+    const urlToCheck  = path.basename(req.url);
+   
+    if (trackedViews.includes(urlToCheck)) {
         const host = req.get("host");
         const origin = req.get("origin") || "none";
         const cookieName = "draftyUnique";
         if(!(cookieName in req.cookies)) {
             const sid = uuidv4();
             res.cookie(cookieName, sid);
-            insertTraffic(url, host, origin, sid);  
+            insertTraffic(req.url, host, origin, sid);  
         } else {
             const sid = req.cookies[cookieName];
-            insertTraffic(url, host, origin, sid);  
+            insertTraffic(req.url, host, origin, sid);  
         }
     }
     next();
