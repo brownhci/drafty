e3eca6e6dad968b982d07f807a7db6a61b301090
Shaun Wallace <shaun_wallace@brown.edu>
2021-01-24 14:03:15 -0500

    add edit history and delete row to nav bar

--- backend/src/public/js/modules/components/sheet/navbar.ts
+++ backend/src/public/js/modules/components/sheet/navbar.ts
@@ -1,17 +1,54 @@
 import { StatusMode, tableFoot } from "./table-foot";
 
+const deleteRowModal: HTMLElement = document.getElementById("deleteRowModal");
+const btnCloseModal: HTMLElement = document.getElementById("btn-modal-close");
 const navBar: HTMLElement = document.getElementById("navbar-nav");
-const navBarAddRow: HTMLElement = document.createElement("li");
-navBarAddRow.innerHTML = `
-            <li class="nav-item">
-                <span id="navbar-addrow">
-                    Add Row
-                </span>
-             </li>
-`;
-navBar.appendChild(navBarAddRow);
 
-navBarAddRow.addEventListener("click", function(event: MouseEvent) {
+const navBarNewRow: HTMLElement = document.createElement("li");
+navBarNewRow.className = "navbar-item";
+navBarNewRow.innerHTML = `
+                        <li class="nav-item">
+                            <span id="navbar-addrow">
+                                Row
+                            </span>
+                        </li>`;
+
+const navBarDelRow: HTMLElement = document.createElement("li");
+navBarDelRow.className = "navbar-item";
+navBarDelRow.innerHTML = `
+                        <li class="nav-item">
+                                <span id="navbar-delrow">
+                                    Row
+                                </span>
+                            </a>
+                        </li>`;
+
+const navBarEditHistory: HTMLElement = document.createElement("li");
+navBarEditHistory.className = "navbar-item";
+navBarEditHistory.innerHTML = `
+                        <li class="nav-item">
+                            <a href="/csprofessors/edit_history">
+                                <span id="navbar-credits">
+                                    Edit History
+                                </span>
+                            </a>
+                        </li>`;
+
+navBar.appendChild(navBarNewRow);
+navBarNewRow.addEventListener("click", function(event: MouseEvent) {
     tableFoot.toggle(StatusMode.Insertion);
     event.stopPropagation();
 }, true);
+
+navBar.appendChild(navBarDelRow);
+navBarDelRow.addEventListener("click", function(event: MouseEvent) {
+    deleteRowModal.style.display = "block";
+    event.stopPropagation();
+}, true);
+
+btnCloseModal.addEventListener("click", function(event: MouseEvent) {
+    deleteRowModal.style.display = "none";
+    event.stopPropagation();
+}, true);
+
+navBar.appendChild(navBarEditHistory);
