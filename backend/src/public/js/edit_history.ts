4bf7582d351d4ee99102e2ceb6687b3b9dd8950a
Shaun Wallace <shaun_wallace@brown.edu>
2021-02-26 00:00:01 -0500

    fix ts loop error on buid

--- backend/src/public/js/edit_history.ts
+++ backend/src/public/js/edit_history.ts
@@ -8,17 +8,17 @@ function cleanDateNumber(val: number, month: boolean = false) {
         converted++;
     }
     if (converted >= 10) {
-        return converted
+        return converted;
     } else {
-        return `0${converted}`
+        return `0${converted}`;
     }
 }
 
 function convertDay(val: number) {
     if (val >= 10) {
-        return val
+        return val;
     } else {
-        return `0${val}`
+        return `0${val}`;
     }
 }
 
@@ -26,13 +26,15 @@ function convertDateToLocalTimezone(date: string) {
     const myDate = new Date(date);
     const convertedDate = new Date(myDate.getTime() - offset * 60000);
     
-    return convertedDate.getFullYear() + '-' + 
-    cleanDateNumber(convertedDate.getMonth(), true) + '-' + 
-    cleanDateNumber(convertedDate.getDate())  + ' ' + 
-    cleanDateNumber(convertedDate.getHours()) + ':' + 
-    cleanDateNumber(convertedDate.getMinutes())
+    return convertedDate.getFullYear() + "-" + 
+    cleanDateNumber(convertedDate.getMonth(), true) + "-" + 
+    cleanDateNumber(convertedDate.getDate())  + " " + 
+    cleanDateNumber(convertedDate.getHours()) + ":" + 
+    cleanDateNumber(convertedDate.getMinutes());
 }
 
-for (let i = 0, row: HTMLTableRowElement; row = editHistoryTable.rows[i]; i++) {
+console.log(editHistoryTable.rows.length);
+for (let i = 1; i < editHistoryTable.rows.length; i++) {
+    const row: HTMLTableRowElement = editHistoryTable.rows[i];
     row.cells[0].innerHTML = convertDateToLocalTimezone(row.cells[0].innerHTML);
- }
+}
