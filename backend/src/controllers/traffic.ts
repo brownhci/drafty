58288ecac67f2aacc31934558b59a5e60c11d692
Shaun Wallace <shaun_wallace@brown.edu>
2021-03-12 11:57:26 -0500

    ignore bots when recording traffic

--- backend/src/controllers/traffic.ts
+++ backend/src/controllers/traffic.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import { v4 as uuidv4 } from "uuid";
 import { insertTraffic } from "../database/traffic";
 import path from "path";
+import { isKnownBot } from "../util/isBot";
 
 const trackedViews = ["","csopenrankings","csprofessors","edit_history","account","login","signup","help"];
 
@@ -9,19 +10,21 @@ const trackedViews = ["","csopenrankings","csprofessors","edit_history","account
  * GLOBAL MIDDLEWARE
  */
 export const trafficLogger = (req: Request, res: Response, next: NextFunction) => {
-    const urlToCheck  = path.basename(req.url);
+    if(!isKnownBot) {
+        const urlToCheck  = path.basename(req.url);
    
-    if (trackedViews.includes(urlToCheck)) {
-        const host = req.get("host");
-        const origin = req.get("origin") || "none";
-        const cookieName = "draftyUnique";
-        if(!(cookieName in req.cookies)) {
-            const sid = uuidv4();
-            res.cookie(cookieName, sid);
-            insertTraffic(req.url, host, origin, sid);  
-        } else {
-            const sid = req.cookies[cookieName];
-            insertTraffic(req.url, host, origin, sid);  
+        if (trackedViews.includes(urlToCheck)) {
+            const host = req.get("host");
+            const origin = req.get("origin") || "none";
+            const cookieName = "draftyUnique";
+            if(!(cookieName in req.cookies)) {
+                const sid = uuidv4();
+                res.cookie(cookieName, sid);
+                insertTraffic(req.url, host, origin, sid);  
+            } else {
+                const sid = req.cookies[cookieName];
+                insertTraffic(req.url, host, origin, sid);  
+            }
         }
     }
     next();
---
+++ backend/src/util/isBot.ts
@@ -0,0 +1,27 @@
+// Map bot name to regular expression used to detect them based
+// on their user-agent
+const knownBotsToPattern = new Map([
+    ["Headless Chrome", /HeadlessChrome/],
+    ["Wget", /[wW]get/],
+    ["Python urllib", /Python-urllib/],
+    ["PHP crawl", /phpcrawl/],
+    ["PhantomJS", /PhantomJS/]
+]);
+
+// Detect if an incoming request belongs to a bot using its user agent
+export function isKnownBot(userAgent: string) {
+    for (const [knownBot, pattern] of knownBotsToPattern.entries()) {
+        if (userAgent.match(pattern)) {
+            return {
+                isBot: true,
+                // In case the request comes from a bot,
+                // we also returns the name of the bot
+                nameBot: knownBot
+            };
+        }
+    }
+
+    return {
+        isBot: false
+    };
+}
