1d906a286fb2c0782f018421df5b87a77dd2f45f
Shaun Wallace <shaun_wallace@brown.edu>
2021-05-14 16:43:47 -0400

    reduce code complexity col suggs

--- backend/src/public/js/modules/components/sheet/column-suggestions.ts
+++ backend/src/public/js/modules/components/sheet/column-suggestions.ts
@@ -3,21 +3,10 @@
  * As the name implies, this module provides autocomplete suggestions for a column. Therefore, it is different from {@link ./suggestions.ts} in that it does not bias a specific cell.
  */
 import { alignElementHorizontally, placeElementAdjacently } from "./align";
-import { columnSuggestionManager, editSuggestionManager } from "./suggestions";
-import { getColumnSuggestionURL, getEditSuggestionURL } from "../../api/endpoints";
-import { getIdSuggestion, getIdSuggestionType } from "../../api/record-interactions";
 import { activeClass } from "../../constants/css-classes";
-import { getCellInTableRow, getEnclosingTableCell } from "../../dom/navigate";
-import { getColumnLabel, isColumnSearchInput, tableHeadSearchElement, tableFootElement } from "../../dom/sheet";
-import { isInput } from "../../dom/types";
-import { FuseSelect } from "../../fuse/sheet-fuse";
+import { tableHeadSearchElement } from "../../dom/sheet";
 import { FuzzySelect } from "../../fuzzy/sheet-fuzzy";
 import { debounce } from "../../utils/debounce";
-import { tableDataManager, updateActiveTableCellElement } from "../../../sheet";
-
-interface Option {
-  suggestion: string;
-}
 
 class ColumnSuggestions {
   private container: HTMLElement = document.getElementById("column-suggestions");
@@ -25,38 +14,10 @@ class ColumnSuggestions {
   private target: HTMLTableCellElement;
   private inputElement: HTMLInputElement;
 
-  private fuseSelect: FuseSelect = new FuseSelect();
   private fuzzySelect: FuzzySelect = new FuzzySelect();
 
   get isActive(): boolean {
     return this.container.classList.contains(activeClass);
-  }
-
-  /**
-   * If suggestions are used for column search, then lower quality suggestions are acceptable.
-   */
-  private get isSuggestionsForColumnSearch(): boolean {
-    return isColumnSearchInput(this.inputElement);
-  }
-
-  private get suggestionIdentifier(): number {
-    const columnIndex = this.target.cellIndex;
-    if (this.isSuggestionsForColumnSearch) {
-      const columnLabel = getColumnLabel(columnIndex);
-      return getIdSuggestionType(columnLabel);
-    } else {
-      const firstRow = tableDataManager.source[0].element_ as HTMLTableRowElement;
-      return getIdSuggestion(getCellInTableRow(firstRow, columnIndex) as HTMLTableCellElement);
-    }
-  }
-
-  private get suggestionFetchURL() {
-    const identifier = this.suggestionIdentifier;
-    if (this.isSuggestionsForColumnSearch) {
-      return getColumnSuggestionURL(identifier);
-    } else {
-      return getEditSuggestionURL(identifier);
-    }
   }
 
   private get containerWidth(): number {
@@ -74,45 +35,45 @@ class ColumnSuggestions {
   }
 
   constructor() {
+
     this.fuzzySelect.handleClickOnOption((text: string) => {
+      console.log("handleClickOnOption()");
       // this dictates what happens when an autocompletion option is clicked
       if (this.inputElement) {
         this.inputElement.value = text;
         this.inputElement.dispatchEvent(new Event("input"));
+
+        console.log(`deactivate --- fuzzy handle click option`);
         this.deactivate();
       }
     });
+
     this.fuzzySelect.mount(element => this.container.appendChild(element));
 
-    tableHeadSearchElement.addEventListener("focus", debounce(this.inputHandler)), true;
+    //tableHeadSearchElement.addEventListener("click", debounce(this.clickHandler)), true;
+    //tableHeadSearchElement.addEventListener("focusin", debounce(this.focusinHandler)), false;
     tableHeadSearchElement.addEventListener("input", debounce(this.inputHandler)), true;
   }
 
-  focusHandler(event: Event) {
-    const target = event.target as HTMLElement;
-    if (columnSuggestions.isActive && target !== columnSuggestions.inputElement) {
-      columnSuggestions.deactivate(); // another element receives focus
-    }
-    if (isInput(target)) {
-      const tableCellElement = getEnclosingTableCell(target);
-      if (tableCellElement) {
-        updateActiveTableCellElement(tableCellElement, false);
-      }
-    }
+  // sw testing
+  /*
+  async clickHandler(event: Event) {
+    console.log("clickHandler()");
+    const eventTarget = event.target as HTMLElement;
+    const target = eventTarget.parentElement as HTMLTableCellElement;
+    await columnSuggestions.fuzzySelect.prePopulateColValues(target.cellIndex);
+    columnSuggestions.activate(target);
   }
+  */
 
   inputHandler(event: Event) {
+    console.log("input handler");
     const target = event.target as HTMLElement;
     if (columnSuggestions.isActive && target === columnSuggestions.inputElement) {
       // the input for searching, filter the suggestions
-
       // FUZZY new
       columnSuggestions.fuzzySelect.query(columnSuggestions.inputElement.value, columnSuggestions.target.cellIndex);
-
-      if (!columnSuggestions.isSuggestionsForColumnSearch) {
-        // if the suggestion window is for column search, then no need to re-align
-        columnSuggestions.align();
-      }
+      columnSuggestions.align();
     }
   }
 
@@ -124,11 +85,6 @@ class ColumnSuggestions {
     this.align();
     // perform query
     this.fuzzySelect.query(this.inputElement.value, this.target.cellIndex);
-    /*
-    this.updateFuseSelect().then(() => {
-      this.fuseSelect.query(this.inputElement.value);
-    });
-    */
     document.body.addEventListener("click", this.handleBodyClick, true);
   }
 
@@ -137,87 +93,18 @@ class ColumnSuggestions {
     document.body.removeEventListener("click", this.handleBodyClick, true);
   }
 
-  callLog(val: string) {
-    console.log(val);
-  }
-
   handleBodyClick(event: Event) {
     const target = event.target as HTMLElement;
-    if (columnSuggestions.isActive && target !== this.inputElement) {
+    if (columnSuggestions.isActive && target !== columnSuggestions.inputElement) {
       columnSuggestions.deactivate();
     }
   }
 
-  private async getSuggestions(
-    handlerForCachedSuggestions?: (options: Array<Option>) => void,
-    handlerForPulledSuggestions?: (options: Array<Option>) => void
-  ) {
-    const forColumnSearch: boolean = this.isSuggestionsForColumnSearch;
-    const suggestionManager = forColumnSearch ? columnSuggestionManager : editSuggestionManager;
-    return await suggestionManager.get(this.suggestionFetchURL, this.suggestionIdentifier.toString(), handlerForCachedSuggestions, (options) => {
-      handlerForPulledSuggestions(options);
-    });
-  }
-
-  private async updateFuseSelect() {
-    //this.fuzzySelect.query(this.inputElement.value, this.target.cellIndex);
-
-    return await this.getSuggestions(
-      /*
-      options => {
-        this.fuzzySelect.query(this.inputElement.value, this.target.cellIndex);
-        this.align();
-      }
-      */
-
-
-      options => {
-        this.fuseSelect.options = options ? options : [];
-        //console.log('options 1 ')
-        this.fuseSelect.sync();
-        this.align();
-      },
-      options => {
-        if (options === null || options.length === 0) {
-          // no autocomplete options to show
-          this.deactivate();
-        } else {
-          //console.log('options 2 ')
-          this.fuseSelect.options = options;
-          this.fuseSelect.sync();
-          this.container.classList.add(activeClass);
-          this.align();
-        }
-      }
-    );
-  }
-
   private align() {
     this.containerWidth = columnSuggestions.inputElement.offsetWidth + 80;
-
     const targetDimensions = this.inputElement.getBoundingClientRect();
     alignElementHorizontally(this.container, targetDimensions);
     placeElementAdjacently(this.container, targetDimensions);
-  }
-
-  /**
-   * Checks whether a provided suggestion is one of the column suggestions for the specified column.
-   *
-   * @param {string} suggestion - The suggestion to test.
-   * @param {number} columnIndex - The index of the column whose suggestions are tested.
-   * @param {HTMLInputElement} [inputElement] - If the provided suggestion comes from a <input> element, provides the input so that if the current column suggestions window is attached to the specified input, the test can be shortened to simply searching the suggestion in `this.fuseSelect`.
-   */
-  async hasSuggestion(suggestion: string, columnIndex: number, inputElement?: HTMLInputElement): Promise<boolean> {
-    if (inputElement && this.inputElement === inputElement) {
-      return this.fuseSelect.hasSuggestion(suggestion);
-    }
-
-    return new Promise(resolve => {
-      this.getSuggestions(
-        options => resolve(options.some(option => option.suggestion === suggestion)),
-        options => resolve(options.some(option => option.suggestion === suggestion))
-      );
-    });
   }
 }
 
