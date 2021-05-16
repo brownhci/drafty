4aca75bce83ede2d95da2c9ce841cedd467af163
Shaun Wallace <shaun_wallace@brown.edu>
2021-05-16 19:35:06 -0400

    sql to track users coming from twitter for databaits

---
+++ util/database/migrations/databaits.sql
@@ -0,0 +1,14 @@
+INSERT INTO `InteractionType` (`idInteractionType`, `interaction`) VALUES (NULL, 'databait_visit');
+
+CREATE TABLE `Databaits` (
+  `idDatabait` int(11) NOT NULL,
+  `idDatabaitTemplate` int(11) NOT NULL,
+  `databait` varchar(1500) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+ALTER TABLE `Databaits`
+  ADD PRIMARY KEY (`idDatabait`);
+
+ALTER TABLE `Databaits`
+  MODIFY `idDatabait` int(11) NOT NULL AUTO_INCREMENT;
