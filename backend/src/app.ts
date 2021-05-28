757ebfc5d50a1b2fb8809d1d8c30467760c90e41
Shaun Wallace <shaun_wallace@brown.edu>
2021-05-28 13:58:27 -0400

    add search col visit api endpoint

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -164,6 +164,7 @@ app.post("/search-partial", interactionCtrl.postSearchPartial);
 app.post("/search-full", interactionCtrl.postSearchFull);
 app.post("/search-google", interactionCtrl.postSearchGoogle);
 app.post("/databait-visit", interactionCtrl.postDataBaitVisit);
+app.post("/searchcol-visit", interactionCtrl.postSearchColVisit);
 app.post("/paste-cell", interactionCtrl.postPasteCell);
 app.post("/copy-cell", interactionCtrl.postCopyCell);
 app.post("/copy-column", interactionCtrl.postCopyColumn);
