76bad117519beff9527f66e23e67f5ec4d280ce7
Shaun Wallace <shaun_wallace@brown.edu>
2021-05-28 11:34:33 -0400

    add fields to databaits table in db

--- util/database/migrations/databaits.sql
+++ util/database/migrations/databaits.sql
@@ -4,6 +4,8 @@ CREATE TABLE `Databaits` (
   `idDatabait` int(11) NOT NULL,
   `idDatabaitTemplate` int(11) NOT NULL,
   `databait` varchar(1500) NOT NULL,
+  `columns` varchar(1500) DEFAULT NULL,
+  `vals` varchar(1500) DEFAULT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
