a8ef669f4890a4499f780e95e393567a423df163
Shaun Wallace <shaun_wallace@brown.edu>
2021-12-07 12:07:32 -0500

    add del row subquery to edit history

--- util/build_edit_history.py
+++ util/build_edit_history.py
@@ -192,6 +192,51 @@ sql_edits_new = """
     order by i.timestamp desc;
 """
 
+sql_edits_del = """
+select us.idProfile,
+       i.idSession,
+       e1.idUniqueID  as idRow, 'del row' as editType, name.suggestion as FullName,
+       uni.suggestion as University,
+       ''          as columnName,
+       'deleted'      as chosen,
+       ''           as previous,
+       i.timestamp
+from Interaction i
+inner join users.Session us on us.idSession = i.idSession
+inner join Edit e on e.IdInteraction = i.idInteraction
+inner join (
+    select es.idEdit, es.idUniqueID
+    from Edit_DelRow es
+) e1 on e1.idEdit = e.idEdit
+         inner join (
+    select s.idSuggestion, s.idUniqueID, s.idSuggestionType, s.suggestion, s.confidence
+    from (
+             select idUniqueID, idSuggestionType, suggestion, confidence, max(confidence) as maxconf
+             from csprofessors.Suggestions
+             where idSuggestionType = 1
+               and active = 1
+             group by idUniqueID, idSuggestionType
+         ) as mx
+             inner join csprofessors.Suggestions s
+                        on s.idUniqueID = mx.idUniqueID and s.idSuggestionType = mx.idSuggestionType and
+                           s.confidence = mx.maxconf
+) name on name.idUniqueID = e1.idUniqueID
+         inner join (
+    select s.idSuggestion, s.idUniqueID, s.idSuggestionType, s.suggestion, s.confidence
+    from (
+             select idUniqueID, idSuggestionType, suggestion, confidence, max(confidence) as maxconf
+             from csprofessors.Suggestions
+             where idSuggestionType = 2
+               and active = 1
+             group by idUniqueID, idSuggestionType
+         ) as mx
+             inner join csprofessors.Suggestions s
+                        on s.idUniqueID = mx.idUniqueID and s.idSuggestionType = mx.idSuggestionType and
+                           s.confidence = mx.maxconf
+) uni on uni.idUniqueID = e1.idUniqueID
+order by i.timestamp desc;
+"""
+
 sql_row_identifier = """
     select s.idSuggestion, s.idUniqueID, s.idSuggestionType, s.suggestion, s.confidence
     from (
