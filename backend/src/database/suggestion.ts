834032598b236bb3d3547da7a2be6bc91025bdf6
Shaun Wallace <shaun_wallace@brown.edu>
2021-12-06 22:34:55 -0500

    delete row model + sql

--- backend/src/database/suggestion.ts
+++ backend/src/database/suggestion.ts
@@ -1,33 +1,35 @@
-import { db, logDbErr } from "./mysql";
+import { db, logDbErr } from './mysql';
 
 /*
 * var used idSuggestion, suggestion, idProfile
 */
-const stmtProcedureEdit: string = "SET @p0=?; SET @p1=?; SET @p2=?; CALL new_suggestion(@p0, @p1, @p2, @p3); SELECT @p0 AS idSuggestion, @p3 AS isNewSuggestion; ";
+const stmtProcedureEdit: string = 'SET @p0=?; SET @p1=?; SET @p2=?; CALL new_suggestion(@p0, @p1, @p2, @p3); SELECT @p0 AS idSuggestion, @p3 AS isNewSuggestion; ';
 
 /*
 * idSuggestionPrev_var, idSuggestionChosen_var, idSession_var, idInteractionType_var, idEntryType_var, mode_var
 */
-const stmtProcedureEditSuggestions: string = "CALL insert_edit_suggestions(?, ?, ?, ?, ?, ?, ?) ";
+const stmtProcedureEditSuggestions: string = 'CALL insert_edit_suggestions(?, ?, ?, ?, ?, ?, ?) ';
 
-const stmtInsertUniqueId: string = "INSERT INTO UniqueId (idUniqueID, active) VALUES (null, 1) ";
+const stmtInsertUniqueId: string = 'INSERT INTO UniqueId (idUniqueID, active) VALUES (null, 1) ';
 
 //INSERT INTO Edit (idInteraction, idEntryType, mode) VALUES (insert_interaction(idSession_var,idInteractionType_var), idEntryType_var, mode_var);
-const stmtInsertInteractionAndEdit: string = "INSERT INTO Edit (idInteraction, idEntryType, mode) VALUES (insert_interaction(?,?), ?, ?) ";
+const stmtInsertInteractionAndEdit: string = 'INSERT INTO Edit (idInteraction, idEntryType, mode) VALUES (insert_interaction(?,?), ?, ?) ';
 
 // suggestion_var, idEdit_var, idProfile_var, idSuggestionType_var, idUniqueId_var
-const stmtInsertNewRowSuggestion: string = "SET @p0=?; SET @p1=?; SET @p2=?; SET @p3=?; SET @p4=?; CALL new_suggestion_new_row(@p0, @p1, @p2, @p3, @p4, @p5);  SELECT @p5 AS idSuggestion_new;";
+const stmtInsertNewRowSuggestion: string = 'SET @p0=?; SET @p1=?; SET @p2=?; SET @p3=?; SET @p4=?; CALL new_suggestion_new_row(@p0, @p1, @p2, @p3, @p4, @p5);  SELECT @p5 AS idSuggestion_new;';
 
 //idProfile_var, idUniqueId_var
-const stmtInsertNewRowSuggestionUserCredit: string = "CALL new_user_credit_suggestion_new_row(?, ?) ";
+const stmtInsertNewRowSuggestionUserCredit: string = 'CALL new_user_credit_suggestion_new_row(?, ?) ';
 
-const stmtSelectSuggestionsWithSuggestionType: string = "SELECT suggestion FROM Suggestions WHERE idSuggestionType = ? AND active = 1 GROUP BY suggestion ORDER BY suggestion asc";
-const stmtSelectSuggestionsForEdit: string = "SELECT suggestion, 1 as prevSugg FROM Suggestions  WHERE idSuggestionType = (SELECT idSuggestionType FROM Suggestions WHERE idSuggestion = ?) AND idUniqueID = (SELECT idUniqueID FROM Suggestions WHERE idSuggestion = ?) "
-  + " UNION "
-  + " SELECT stv.value as suggestion, 0 as prevSugg  FROM (SELECT * FROM SuggestionTypeValues WHERE active = 1) stv WHERE stv.idSuggestionType = (SELECT idSuggestionType FROM Suggestions WHERE idSuggestion = ?) "
-  + " ORDER BY prevSugg DESC, suggestion ASC ";
+const stmtInsertDelRow: string = 'INSERT INTO Edit_DelRow (idInteraction, idUniqueID, comment) VALUES (insert_interaction(?,?), ?, ?)';
 
-const stmtGetSuggestionTypeValidationRule: string = "SELECT idSuggestionType, regex FROM SuggestionType;";
+const stmtSelectSuggestionsWithSuggestionType: string = 'SELECT suggestion FROM Suggestions WHERE idSuggestionType = ? AND active = 1 GROUP BY suggestion ORDER BY suggestion asc';
+const stmtSelectSuggestionsForEdit: string = 'SELECT suggestion, 1 as prevSugg FROM Suggestions  WHERE idSuggestionType = (SELECT idSuggestionType FROM Suggestions WHERE idSuggestion = ?) AND idUniqueID = (SELECT idUniqueID FROM Suggestions WHERE idSuggestion = ?) '
+  + ' UNION '
+  + ' SELECT stv.value as suggestion, 0 as prevSugg  FROM (SELECT * FROM SuggestionTypeValues WHERE active = 1) stv WHERE stv.idSuggestionType = (SELECT idSuggestionType FROM Suggestions WHERE idSuggestion = ?) '
+  + ' ORDER BY prevSugg DESC, suggestion ASC ';
+
+const stmtGetSuggestionTypeValidationRule: string = 'SELECT idSuggestionType, regex FROM SuggestionType;';
 
 /**
  * returns current or new idSuggestion
@@ -39,13 +41,13 @@ export async function newSuggestion(idSuggestion: number, suggestion: string, id
     //idSuggestionPrev_var, idSuggestionChosen_var, idSession_var, idInteractionType_var, idEntryType_var, mode_var
     const idSuggestionPrev = idSuggestion;
     const pos = Object.keys(results).pop(); // get last
-    const idSuggestionChosen = (results as any)[pos][0]["idSuggestion"]; // sw: this is bc of how procedures return data
+    const idSuggestionChosen = (results as any)[pos][0]['idSuggestion']; // sw: this is bc of how procedures return data
 
     db.query(stmtProcedureEditSuggestions, [idSuggestionPrev, idSuggestionChosen, idSession, idInteractionType, idEntryType, mode, idProfile]);
 
     return [null, idSuggestionChosen];
   } catch (error) {
-    logDbErr(error, "error during newSuggestion procedure", "warn");
+    logDbErr(error, 'error during newSuggestion procedure', 'warn');
     return [error];
   }
 }
@@ -60,7 +62,7 @@ export async function insertNewRowId() {
     const [results] = await db.query(stmtInsertUniqueId);
     return [null, results];
   } catch (error) {
-    logDbErr(error, "error during insert row (UniqueID)", "warn");
+    logDbErr(error, 'error during insert row (UniqueID)', 'warn');
     return error;
   }
 }
@@ -77,7 +79,7 @@ export async function insertInteractionAndEdit(idSession: number, idInteractionT
     return [null, results];
   } catch (error) {
     console.log(error);
-    logDbErr(error, "error during insertInteractionAndEdit", "warn");
+    logDbErr(error, 'error during insertInteractionAndEdit', 'warn');
     return error;
   }
 }
@@ -96,13 +98,13 @@ export async function insertNewRowSuggestion(suggestion: string, idEdit: number,
     console.log(results);
 
     const pos = Object.keys(results).pop(); // get last
-    const idSuggestionNew = (results as any)[pos][0]["idSuggestion_new"]; // sw: this is bc of how procedures return data
+    const idSuggestionNew = (results as any)[pos][0]['idSuggestion_new']; // sw: this is bc of how procedures return data
     //console.log("idSuggestionNew = " + idSuggestionNew);
 
     return idSuggestionNew;
   } catch (error) {
     console.log(error);
-    logDbErr(error, "error during insertNewRowSuggestion", "warn");
+    logDbErr(error, 'error during insertNewRowSuggestion', 'warn');
   }
 }
 
@@ -117,7 +119,22 @@ export async function insertNewRowSuggestionUserCredit(idProfile: number, idUniq
     db.query(stmtInsertNewRowSuggestionUserCredit, [idProfile, idUniqueId]);
   } catch (error) {
     console.log(error);
-    logDbErr(error, "error during insertNewRowSuggestionUserCredit", "warn");
+    logDbErr(error, 'error during insertNewRowSuggestionUserCredit', 'warn');
+  }
+}
+
+/***
+*
+* Only used for Del Row
+*
+***/
+export async function deactivateRow(idSession: number, idUniqueID: string, comment: string) {
+  try {
+    const idInteractionType: number = 17;
+    db.query(stmtInsertDelRow, [idSession, idInteractionType, idUniqueID, comment]);
+  } catch (error) {
+    console.log(error);
+    logDbErr(error, 'error during deactivateRow', 'warn');
   }
 }
 
@@ -131,7 +148,7 @@ export async function selectSuggestionsForEdit(idSuggestion: number) {
     const [results] = await db.query(stmtSelectSuggestionsForEdit, [idSuggestion, idSuggestion, idSuggestion]);
     return [null, results];
   } catch (error) {
-    logDbErr(error, "error during selectSuggestionsForEdit", "warn");
+    logDbErr(error, 'error during selectSuggestionsForEdit', 'warn');
     return [error];
   }
 }
@@ -145,7 +162,7 @@ export async function getValidationRule() {
     const [results] = await db.query(stmtGetSuggestionTypeValidationRule);
     return [null, results];
   } catch (error) {
-    logDbErr(error, "error during retrieving validation rules", "warn");
+    logDbErr(error, 'error during retrieving validation rules', 'warn');
     return [error];
   }
 }
@@ -163,7 +180,7 @@ export async function getSuggestionsWithSuggestionType(idSuggestionType: number)
     const [results] = await db.query(stmtSelectSuggestionsWithSuggestionType, [idSuggestionType]);
     return [null, results];
   } catch (error) {
-    logDbErr(error, "error during fetching suggestions", "warn");
+    logDbErr(error, 'error during fetching suggestions', 'warn');
     return [error];
   }
 }
