2962ace29a8461016c6514277d7f1647195645ce
swallace21 <shaun_wallace@brown.edu>
2022-02-06 10:56:09 -0500

    add Did you know to navbar

--- backend/src/public/js/modules/components/sheet/navbar.ts
+++ backend/src/public/js/modules/components/sheet/navbar.ts
@@ -37,6 +37,15 @@ navBarEditHistory.innerHTML = `
                             </a>
                         </li>`;
 
+const navBarDidYouKnow: HTMLElement = document.createElement('li');
+navBarDidYouKnow.className = 'navbar-item';
+navBarDidYouKnow.innerHTML = `
+                        <li class="nav-item">
+                            <span id="navbar-did-you-know">
+                                Did you know
+                            </span>
+                        </li>`;
+
 navBar.appendChild(navBarEditHistory);
 
 navBar.appendChild(navBarNewRow);
@@ -55,6 +64,11 @@ navBarDelRow.addEventListener('click', function(event: MouseEvent) {
     event.stopPropagation();
 }, true);
 
+navBar.appendChild(navBarDidYouKnow);
+navBarDidYouKnow.addEventListener('click', function(event: MouseEvent) {
+    // TODO create random did you know
+    event.stopPropagation();
+}, true);
 
 btnCloseModaError.addEventListener('click', function(event: MouseEvent) {
     deleteRowModalError.style.display = 'none';
