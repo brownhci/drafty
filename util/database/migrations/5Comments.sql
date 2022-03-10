914a02d130f739f06de5384a6192c8b16bada6d9
swallace21 <shaun_wallace@brown.edu>
2022-03-10 15:04:52 -0500

    comments feature database migration

---
+++ util/database/migrations/5Comments.sql
@@ -0,0 +1,36 @@
+create table Comments
+(
+    idComment     int                                 auto_increment,
+    idInteraction int                                 not null,
+    idUniqueID    int                                 not null,
+    comment       longtext                            not null,
+    voteUp        int default 0                       not null,
+    voteDown      int default 0                       not null,
+    constraint Comments_pk
+        primary key (idComment),
+    constraint Comments___fk_idInteraction_ksjdfba87aidsb
+        foreign key (idInteraction) references Interaction (idInteraction),
+    constraint Comments___fk_iduniqid_oq83eyfgqwuyofhba
+        foreign key (idUniqueID) references UniqueId (idUniqueID)
+);
+
+INSERT INTO csprofessors.InteractionType (idInteractionType, interaction) VALUES (19,'comment');
+INSERT INTO csprofessors.InteractionType (idInteractionType, interaction) VALUES (20,'commentVoteUp');
+INSERT INTO csprofessors.InteractionType (idInteractionType, interaction) VALUES (21,'commentVoteDown');
+INSERT INTO csprofessors.InteractionType (idInteractionType, interaction) VALUES (22,'commentVoteUp-deselect');
+INSERT INTO csprofessors.InteractionType (idInteractionType, interaction) VALUES (23,'commentVoteDown-deselect');
+
+create table CommentVote
+(
+    idCommentVote int auto_increment,
+    idInteraction int     not null,
+    idComment     int     not null,
+    vote          varchar(20) not null,
+    constraint CommentVote_pk
+        primary key (idCommentVote),
+    constraint table_name___fk_comments_akhsdfashjld
+        foreign key (idComment) references Comments (idComment),
+    constraint table_name___fk_interaction_akdhfa
+        foreign key (idInteraction) references Interaction (idInteraction),
+    constraint voteType check (vote in ('voteUp','voteUp-deselect','voteDown','voteDown-select'))
+);
