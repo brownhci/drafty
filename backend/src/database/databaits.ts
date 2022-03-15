5cc065c201c7a121c743447d50300ea16f19e11c
swallace21 <shaun_wallace@brown.edu>
2022-03-15 15:59:44 -0400

    create databaitAction typescript Type

--- backend/src/database/databaits.ts
+++ backend/src/database/databaits.ts
@@ -1,2 +1,23 @@
 import { db, logDbErr } from './mysql';
 
+// stmtInsertDatabait
+// stmtUpdateDatabaitClosed
+// stmtUpdateDatabaitNextAction
+// stmtInsertDatabaitTweet
+// stmtUpdateDatabaitTweetNextAction
+// stmtUpdateDatabaitTweetLikes
+// stmtUpdateDatabaitTweetRetweets
+
+export const databaitAction = {
+    tweet: 'tweet',
+    rightClick: 'right-click', 
+    edit: 'edit', 
+    newRow: 'new-row', 
+    deleteRow: 'delete-row', 
+    navbarMenu: 'navbar-menu', 
+    modalLike: 'modal-like', 
+    modalRandom: 'modal-random'
+} as const;
+
+/* TS magic to allow flexible lookup */
+export type databaitAction = typeof databaitAction [ keyof typeof databaitAction ]
