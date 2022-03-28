27d3c9e9976d225caf70801a3cf768bcb76d9cac
swallace21 <shaun_wallace@brown.edu>
2022-03-28 10:22:50 -0400

    move navbar menu to handlebars so they are server side rendered

--- backend/src/public/js/modules/components/sheet/navbar.ts
+++ backend/src/public/js/modules/components/sheet/navbar.ts
@@ -4,59 +4,12 @@ import { activateDataBait } from './databaits';
 import { activeTableCellElement } from '../../../sheet';
 import { DatabaitCreateType, InteractionTypeDatabaitCreate } from '../../../../../types/databaits';
 
-const navBar: HTMLElement = document.getElementById('navbar-nav');
-
 const deleteRowModalError: HTMLElement = document.getElementById('deleteRowModal_error');
 const btnCloseModalError: HTMLElement = document.getElementById('btn-delrow-modal-close');
 
-const navBarNewRow: HTMLElement = document.createElement('li');
-navBarNewRow.className = 'navbar-item';
-navBarNewRow.innerHTML = `
-                        <li class="nav-item">
-                            <a href="#" class="nav-label">
-                                <span id="navbar-addrow">
-                                    Add Row
-                                </span>
-                            </a>
-                        </li>`;
-
-const navBarDelRow: HTMLElement = document.createElement('li');
-navBarDelRow.className = 'navbar-item';
-navBarDelRow.innerHTML = `
-                        <li class="nav-item">
-                            <a href="#" class="nav-label">
-                                <span id="navbar-delrow">
-                                    Delete Row
-                                </span>
-                            </a>
-                        </li>`;
-
-const navBarEditHistory: HTMLElement = document.createElement('li');
-navBarEditHistory.className = 'navbar-item';
-navBarEditHistory.innerHTML = `
-                        <li class="nav-item">
-                            <a href="/csprofessors/edit_history" class="nav-label">
-                                <span id="navbar-credits">
-                                    Edit History
-                                </span>
-                            </a>
-                        </li>`;
-
-const navBarDidYouKnow: HTMLElement = document.createElement('li');
-navBarDidYouKnow.className = 'navbar-item';
-navBarDidYouKnow.innerHTML = `
-                        <li class="nav-item">
-                            <a href="#" class="nav-label">
-                                <span id="navbar-didyouknow">
-                                    Did you know
-                                </span>
-                            </a>
-                        </li>`;
-
-navBar.appendChild(navBarEditHistory);
-navBar.appendChild(navBarNewRow);
-navBar.appendChild(navBarDelRow);
-navBar.appendChild(navBarDidYouKnow);
+const navBarNewRow: HTMLElement = document.getElementById('navbar-addrow-select');
+const navBarDelRow: HTMLElement = document.getElementById('navbar-delrow-select');
+const navBarDidYouKnow: HTMLElement = document.getElementById('navbar-didyouknow-select');
 
 navBarNewRow.addEventListener('click', function(event: MouseEvent) {
     tableFoot.toggle(StatusMode.Insertion);
