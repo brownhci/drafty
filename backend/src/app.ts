c6c1a4cac0c9664a95e0f9cf921a62e0570c2523
Shaun Wallace <shaun_wallace@brown.edu>
2022-04-11 21:52:37 -0400

    removed unused endpoint from api -> postDatabaitCreated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -174,7 +174,6 @@ app.get('/data/csv/:name/:token', dataSharingCtrl.getCSV);
 // databaits + twitter
 app.post('/databaits/tweet', databaitsCtrl.postTweet);
 app.post('/databaits/tweet/next', databaitsCtrl.postTweetNextAction);
-app.post('/databaits/create', databaitsCtrl.postDatabaitCreated);
 app.post('/databaits/next', databaitsCtrl.postDatabaitNextAction);
 app.post('/databaits/visit', databaitsCtrl.postDatabaitVisit);
 
