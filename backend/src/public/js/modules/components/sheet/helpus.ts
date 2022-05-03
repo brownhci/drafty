3adb9edd0e776116d2dd6cc205c141eb3dbab87b
kaki1104 <kaki.so2011@gmail.com>
2022-05-03 01:28:21 -0400

    help us shows up in navbar

---
+++ backend/src/public/js/modules/components/sheet/helpus.ts
@@ -0,0 +1,18 @@
+const helpUsModal: HTMLElement = document.getElementById('helpus-screen');
+const loadingHTML: string = `Creating something awesome...`;
+const helpUsText: HTMLElement = document.getElementById('helpus-text');
+
+
+function openModal() {
+    helpUsText.innerHTML = loadingHTML;
+    // eslint-disable-next-line @typescript-eslint/no-use-before-define
+    // activateKeyListener();
+    helpUsModal.style.display = 'block';
+}
+
+export async function activateHelpUs(tableCellElement: HTMLTableCellElement) {
+    // const baseUrl: urlBase = { idInteractionType: idInteractionType, idDatabaitCreateType: idDatabaitCreateType, idSession: await getIdSession()};
+    // resetContributionMessageHTML();
+    openModal();
+}
+
